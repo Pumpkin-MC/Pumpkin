09af356b0aabfcaf82c0d9d106c0c7bb73e641dc
Dominik <80042910+tomasalias@users.noreply.github.com>
2025-01-06 19:44:32 +0100

    Update c_entity_velocity.rs

--- pumpkin-protocol/src/client/play/c_entity_velocity.rs
+++ pumpkin-protocol/src/client/play/c_entity_velocity.rs
@@ -16,9 +16,9 @@ impl<'a> CEntityVelocity<'a> {
     pub fn new(entity_id: &'a VarInt, velocity_x: f64, velocity_y: f64, velocity_z: f64) -> Self {
         Self {
             entity_id,
-            velocity_x: (velocity.x.clamp(-3.9, 3.9) * 8000.0) as i16,
-            velocity_y: (velocity.y.clamp(-3.9, 3.9) * 8000.0) as i16,
-            velocity_z: (velocity.z.clamp(-3.9, 3.9) * 8000.0) as i16,
+            velocity_x: (velocity_x.clamp(-3.9, 3.9) * 8000.0) as i16,
+            velocity_y: (velocity_y.clamp(-3.9, 3.9) * 8000.0) as i16,
+            velocity_z: (velocity_z.clamp(-3.9, 3.9) * 8000.0) as i16,
         }
     }
 }
