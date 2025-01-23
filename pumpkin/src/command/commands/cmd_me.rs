1a1062dc51c83783b9366871eaf8d18c7bf89344
Alexander Medvedev <lilalexmed@proton.me>
2025-01-23 21:34:39 +0100

    fixed everything and port to chat types

--- pumpkin/src/command/commands/cmd_me.rs
+++ pumpkin/src/command/commands/cmd_me.rs
@@ -1,15 +1,12 @@
 use async_trait::async_trait;
-use pumpkin_core::text::TextComponent;
-use pumpkin_protocol::client::play::CSystemChatMessage;
+use pumpkin_data::world::EMOTE_COMMAND;
+use pumpkin_util::text::TextComponent;
 
-use crate::{
-    command::{
-        args::{arg_message::MsgArgConsumer, Arg, ConsumedArgs},
-        tree::CommandTree,
-        tree_builder::{argument, require},
-        CommandError, CommandExecutor, CommandSender,
-    },
-    entity::player::PermissionLvl,
+use crate::command::{
+    args::{arg_message::MsgArgConsumer, Arg, ConsumedArgs},
+    tree::CommandTree,
+    tree_builder::argument,
+    CommandError, CommandExecutor, CommandSender,
 };
 use CommandError::InvalidConsumption;
 
@@ -29,29 +26,23 @@ impl CommandExecutor for MeExecutor {
         server: &crate::server::Server,
         args: &ConsumedArgs<'a>,
     ) -> Result<(), CommandError> {
-        let sender = match sender {
-            CommandSender::Console => "Server",
-            CommandSender::Rcon(_) => "Rcon",
-            CommandSender::Player(player) => &player.gameprofile.name,
-        };
-
         let Some(Arg::Msg(msg)) = args.get(ARG_MESSAGE) else {
             return Err(InvalidConsumption(Some(ARG_MESSAGE.into())));
         };
 
         server
-            .broadcast_packet_all(&CSystemChatMessage::new(
-                &TextComponent::text(&format!("* {sender} {msg}")),
-                false,
-            ))
+            .broadcast_message(
+                &TextComponent::text(msg.clone()),
+                &TextComponent::text(format!("{sender}")),
+                EMOTE_COMMAND,
+                None,
+            )
             .await;
         Ok(())
     }
 }
 
-pub fn init_command_tree<'a>() -> CommandTree<'a> {
-    CommandTree::new(NAMES, DESCRIPTION).with_child(
-        require(&|sender| sender.has_permission_lvl(PermissionLvl::Zero))
-            .with_child(argument(ARG_MESSAGE, &MsgArgConsumer).execute(&MeExecutor)),
-    )
+pub fn init_command_tree() -> CommandTree {
+    CommandTree::new(NAMES, DESCRIPTION)
+        .with_child(argument(ARG_MESSAGE, MsgArgConsumer).execute(MeExecutor))
 }
