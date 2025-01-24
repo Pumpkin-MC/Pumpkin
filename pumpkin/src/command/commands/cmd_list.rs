4d99219277ae884d66696661853435a1e14366f6
Melther <MrMewtho@gmail.com>
2025-01-24 04:40:13 +0100

    Adapted translations
    Changed all the translate components to fit the new format, also added translation to gamemode command.

--- pumpkin/src/command/commands/cmd_list.rs
+++ pumpkin/src/command/commands/cmd_list.rs
@@ -31,10 +31,10 @@ impl CommandExecutor for ListExecutor {
             .send_message(TextComponent::translate(
                 "commands.list.players",
                 [
-                    players.len().to_string().into(),
-                    BASIC_CONFIG.max_players.to_string().into(),
-                    get_player_names(players).into(),
-                ],
+                    TextComponent::text(players.len().to_string()),
+                    TextComponent::text(BASIC_CONFIG.max_players.to_string()),
+                    TextComponent::text(get_player_names(players)),
+                ].into(),
             ))
             .await;
 
