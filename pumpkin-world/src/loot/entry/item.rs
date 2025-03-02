c1fc1c96be6a19c9dd2d39294f615a790a7dfae7
Alexander Medvedev <lilalexmed@proton.me>
2025-03-02 14:41:05 +0100

    make Loot Tables work

--- pumpkin-world/src/loot/entry/item.rs
+++ pumpkin-world/src/loot/entry/item.rs
@@ -1,7 +1,16 @@
+use pumpkin_data::item::Item;
 use serde::Deserialize;
 
+use crate::item::ItemStack;
+
 #[derive(Deserialize, Clone)]
-#[expect(dead_code)]
 pub struct ItemEntry {
     name: String,
+}
+
+impl ItemEntry {
+    pub fn get_items(&self) -> Vec<ItemStack> {
+        let item = Item::from_registry_key(&self.name.replace("minecraft:", "")).unwrap();
+        vec![ItemStack::new(1, item)]
+    }
 }
--- pumpkin-world/src/loot/entry/mod.rs
+++ pumpkin-world/src/loot/entry/mod.rs
@@ -1,11 +1,14 @@
+use alternative::AlternativeEntry;
 use item::ItemEntry;
 use serde::Deserialize;
 
+use crate::item::ItemStack;
+
+mod alternative;
 mod item;
 
 #[derive(Deserialize, Clone)]
 #[serde(tag = "type")]
-#[expect(dead_code)]
 pub enum LootPoolEntryTypes {
     #[serde(rename = "minecraft:empty")]
     Empty,
@@ -18,9 +21,24 @@ pub enum LootPoolEntryTypes {
     #[serde(rename = "minecraft:tag")]
     Tag,
     #[serde(rename = "minecraft:alternatives")]
-    Alternatives,
+    Alternatives(AlternativeEntry),
     #[serde(rename = "minecraft:sequence")]
     Sequence,
     #[serde(rename = "minecraft:group")]
     Group,
+}
+
+impl LootPoolEntryTypes {
+    pub fn get_items(&self) -> Vec<ItemStack> {
+        match self {
+            LootPoolEntryTypes::Empty => todo!(),
+            LootPoolEntryTypes::Item(item_entry) => item_entry.get_items(),
+            LootPoolEntryTypes::LootTable => todo!(),
+            LootPoolEntryTypes::Dynamic => todo!(),
+            LootPoolEntryTypes::Tag => todo!(),
+            LootPoolEntryTypes::Alternatives(alternative) => alternative.get_items(),
+            LootPoolEntryTypes::Sequence => todo!(),
+            LootPoolEntryTypes::Group => todo!(),
+        }
+    }
 }
