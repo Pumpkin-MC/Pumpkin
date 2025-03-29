5239dffed8df3d3d28fd44cc39e2553c0373716a
Alexander Medvedev <lilalexmed@proton.me>
2025-03-29 18:57:51 +0100

    change biomes parsing
    now we have more infos

--- pumpkin-data/build/biome.rs
+++ pumpkin-data/build/biome.rs
@@ -1,26 +1,113 @@
-use heck::ToPascalCase;
-use proc_macro2::TokenStream;
+use std::collections::HashMap;
+
+use heck::ToShoutySnakeCase;
+use proc_macro2::{Span, TokenStream};
 use quote::{format_ident, quote};
+use serde::Deserialize;
+use syn::LitInt;
+
+#[derive(Deserialize)]
+pub struct Biome {
+    has_precipitation: bool,
+    temperature: f32,
+    downfall: f32,
+    //carvers: Vec<String>,
+    features: Vec<Vec<String>>,
+}
 
 pub(crate) fn build() -> TokenStream {
     println!("cargo:rerun-if-changed=../assets/biome.json");
 
-    let biomes: Vec<String> = serde_json::from_str(include_str!("../../assets/biome.json"))
-        .expect("Failed to parse biome.json");
+    let biomes: HashMap<String, Biome> =
+        serde_json::from_str(include_str!("../../assets/biome.json"))
+            .expect("Failed to parse biome.json");
     let mut variants = TokenStream::new();
+    let mut type_to_name = TokenStream::new();
+    let mut name_to_type = TokenStream::new();
+    let mut type_to_id = TokenStream::new();
 
-    for status in biomes.iter() {
-        let full_name = format!("minecraft:{status}");
-        let name = format_ident!("{}", status.to_pascal_case());
+    for (i, (name, biome)) in biomes.iter().enumerate() {
+        // let full_name = format!("minecraft:{name}");
+        let format_name = format_ident!("{}", name.to_shouty_snake_case());
+        let has_precipitation = biome.has_precipitation;
+        let temperature = biome.temperature;
+        let downfall = biome.downfall;
+        //  let carvers = &biome.carvers;
+        let features = &biome.features;
+
         variants.extend([quote! {
-            #[serde(rename = #full_name)]
-            #name,
+            pub const #format_name: Biome = Biome {
+               has_precipitation: #has_precipitation,
+               temperature: #temperature,
+               downfall: #downfall,
+              features: &[#(&[#(#features),*]),*]
+            };
         }]);
+
+        type_to_name.extend(quote! { Self::#format_name => #name, });
+        name_to_type.extend(quote! { #name => Some(Self::#format_name), });
+        let index = LitInt::new(&i.to_string(), Span::call_site());
+        type_to_id.extend(quote! { Self::#format_name => #index, });
     }
+
     quote! {
-        #[derive(Clone, Deserialize, Copy, Hash, PartialEq, Eq, Debug)]
-        pub enum Biome {
+        use serde::{de, Deserializer};
+        use std::fmt;
+
+        #[derive(Clone, Copy, PartialEq, Debug)]
+        pub struct Biome {
+            has_precipitation: bool,
+            temperature: f32,
+            downfall: f32,
+           // carvers: &'static [&str],
+            features: &'static [&'static [&'static str]]
+        }
+
+        impl<'de> Deserialize<'de> for Biome {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                struct BiomeVisitor;
+
+                impl de::Visitor<'_> for BiomeVisitor {
+                    type Value = Biome;
+
+                    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                        formatter.write_str("a biome name as a string")
+                    }
+
+                    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        self.visit_str(&v)
+                    }
+
+                    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+                    where
+                        E: de::Error,
+                    {
+                        let biome = Biome::from_name(&value.replace("minecraft:", ""));
+
+                        biome.ok_or_else(|| E::unknown_variant(value, &["unknown biome"]))
+                    }
+                }
+
+                deserializer.deserialize_str(BiomeVisitor)
+            }
+        }
+
+        impl Biome {
             #variants
+
+            pub  fn from_name(name: &str) -> Option<Self> {
+                          match name {
+                              #name_to_type
+                              _ => None
+
+                          }
+                      }
         }
     }
 }
