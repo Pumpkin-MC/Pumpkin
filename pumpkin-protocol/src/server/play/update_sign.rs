c73fb79d1c0ea74c471b7d3873cf0e57a0e826c5
BigPebble123 <>
2025-04-05 07:11:08 +0200

    SUpdateSign real length verification

--- pumpkin-protocol/src/server/play/update_sign.rs
+++ pumpkin-protocol/src/server/play/update_sign.rs
@@ -3,6 +3,7 @@ use std::io::Read;
 use pumpkin_data::packet::serverbound::PLAY_SIGN_UPDATE;
 use pumpkin_macros::packet;
 use pumpkin_util::math::position::BlockPos;
+use pumpkin_config::BASIC_CONFIG;
 
 use crate::{
     ServerPacket,
@@ -19,19 +20,23 @@ pub struct SUpdateSign {
     pub line_4: String, // 384
 }
 
-const MAX_LINE_LENGTH: usize = 386;
-
 impl ServerPacket for SUpdateSign {
     fn read(read: impl Read) -> Result<Self, ReadingError> {
         let mut read = read;
 
+        let max_line_length: usize = if BASIC_CONFIG.allow_impossible_actions {
+            386
+        } else {
+            15
+        };
+
         Ok(Self {
             location: BlockPos::from_i64(read.get_i64_be()?),
             is_front_text: read.get_bool()?,
-            line_1: read.get_string_bounded(MAX_LINE_LENGTH)?,
-            line_2: read.get_string_bounded(MAX_LINE_LENGTH)?,
-            line_3: read.get_string_bounded(MAX_LINE_LENGTH)?,
-            line_4: read.get_string_bounded(MAX_LINE_LENGTH)?,
+            line_1: read.get_string_bounded(max_line_length)?,
+            line_2: read.get_string_bounded(max_line_length)?,
+            line_3: read.get_string_bounded(max_line_length)?,
+            line_4: read.get_string_bounded(max_line_length)?,
         })
     }
 }
