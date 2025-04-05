65dbce666045a3aabb1c08db5ef5386e0dadcbe0
tetram2674562 <aymericgab77@gmail.com>
2025-04-05 12:18:47 +0200

    Feat : Added stripping on logs

--- pumpkin/src/item/items/axe.rs
+++ pumpkin/src/item/items/axe.rs
@@ -3,13 +3,43 @@ use crate::item::pumpkin_item::{ItemMetadata, PumpkinItem};
 use crate::server::Server;
 use crate::world::BlockFlags;
 use async_trait::async_trait;
-use pumpkin_data::block::Block;
+use lazy_static::lazy_static;
+use pumpkin_data::block::{Block, BlockProperties, PaleOakWoodLikeProperties};
 use pumpkin_data::item::Item;
 use pumpkin_data::tag::Tagable;
 use pumpkin_util::math::position::BlockPos;
 use pumpkin_world::block::BlockDirection;
+use std::collections::HashMap;
 pub struct AxeItem;
 
+// Need review. Since I can't verify if a block is equal to another (like mojang did) I'm going to do it with there id.
+lazy_static! {
+    static ref STRIPPED_BLOCKS: HashMap<u16, Block> = HashMap::from([
+        (Block::OAK_WOOD.id, Block::STRIPPED_OAK_WOOD),
+        (Block::OAK_LOG.id, Block::STRIPPED_OAK_LOG),
+        (Block::DARK_OAK_WOOD.id, Block::STRIPPED_DARK_OAK_WOOD),
+        (Block::DARK_OAK_LOG.id, Block::STRIPPED_DARK_OAK_LOG),
+        (Block::PALE_OAK_WOOD.id, Block::STRIPPED_PALE_OAK_WOOD),
+        (Block::PALE_OAK_LOG.id, Block::STRIPPED_PALE_OAK_LOG),
+        (Block::ACACIA_WOOD.id, Block::STRIPPED_ACACIA_WOOD),
+        (Block::ACACIA_LOG.id, Block::STRIPPED_ACACIA_LOG),
+        (Block::CHERRY_WOOD.id, Block::STRIPPED_CHERRY_WOOD),
+        (Block::CHERRY_LOG.id, Block::STRIPPED_CHERRY_LOG),
+        (Block::BIRCH_WOOD.id, Block::STRIPPED_BIRCH_WOOD),
+        (Block::BIRCH_LOG.id, Block::STRIPPED_BIRCH_LOG),
+        (Block::JUNGLE_WOOD.id, Block::STRIPPED_JUNGLE_WOOD),
+        (Block::JUNGLE_LOG.id, Block::STRIPPED_JUNGLE_LOG),
+        (Block::SPRUCE_WOOD.id, Block::STRIPPED_SPRUCE_WOOD),
+        (Block::SPRUCE_LOG.id, Block::STRIPPED_SPRUCE_LOG),
+        (Block::WARPED_STEM.id, Block::STRIPPED_WARPED_STEM),
+        (Block::WARPED_HYPHAE.id, Block::STRIPPED_WARPED_HYPHAE),
+        (Block::CRIMSON_STEM.id, Block::STRIPPED_CRIMSON_STEM),
+        (Block::CRIMSON_HYPHAE.id, Block::STRIPPED_CRIMSON_HYPHAE),
+        (Block::MANGROVE_WOOD.id, Block::STRIPPED_MANGROVE_WOOD),
+        (Block::MANGROVE_LOG.id, Block::STRIPPED_MANGROVE_LOG),
+        (Block::BAMBOO_BLOCK.id, Block::STRIPPED_BAMBOO_BLOCK)
+    ]);
+}
 impl ItemMetadata for AxeItem {
     fn ids() -> Box<[u16]> {
         Item::get_tag_values("#minecraft:axes")
@@ -33,194 +63,34 @@ impl PumpkinItem for AxeItem {
         _item: &Item,
         player: &Player,
         location: BlockPos,
-        _face: &BlockDirection,
+        face: &BlockDirection,
         block: &Block,
         _server: &Server,
     ) {
-        let replacement_block = match &block.id {
-            id if id == &Block::OAK_LOG.id => Block::STRIPPED_OAK_LOG.default_state_id,
-            id if id == &Block::BIRCH_LOG.id => Block::STRIPPED_BIRCH_LOG.default_state_id,
-            id if id == &Block::SPRUCE_LOG.id => Block::STRIPPED_SPRUCE_LOG.default_state_id,
-            id if id == &Block::ACACIA_LOG.id => Block::STRIPPED_ACACIA_LOG.default_state_id,
-            id if id == &Block::CHERRY_LOG.id => Block::STRIPPED_CHERRY_LOG.default_state_id,
-            id if id == &Block::JUNGLE_LOG.id => Block::STRIPPED_JUNGLE_LOG.default_state_id,
-            id if id == &Block::PALE_OAK_LOG.id => Block::STRIPPED_PALE_OAK_LOG.default_state_id,
-            id if id == &Block::DARK_OAK_LOG.id => Block::STRIPPED_DARK_OAK_LOG.default_state_id,
-            id if id == &Block::MANGROVE_LOG.id => Block::STRIPPED_MANGROVE_LOG.default_state_id,
-            id if id == &Block::OAK_WOOD.id => Block::STRIPPED_OAK_WOOD.default_state_id,
-            id if id == &Block::BIRCH_WOOD.id => Block::STRIPPED_BIRCH_WOOD.default_state_id,
-            id if id == &Block::SPRUCE_WOOD.id => Block::STRIPPED_SPRUCE_WOOD.default_state_id,
-            id if id == &Block::ACACIA_WOOD.id => Block::STRIPPED_ACACIA_WOOD.default_state_id,
-            id if id == &Block::CHERRY_WOOD.id => Block::STRIPPED_CHERRY_WOOD.default_state_id,
-            id if id == &Block::JUNGLE_WOOD.id => Block::STRIPPED_JUNGLE_WOOD.default_state_id,
-            id if id == &Block::PALE_OAK_WOOD.id => Block::STRIPPED_PALE_OAK_WOOD.default_state_id,
-            id if id == &Block::DARK_OAK_WOOD.id => Block::STRIPPED_DARK_OAK_WOOD.default_state_id,
-            id if id == &Block::MANGROVE_WOOD.id => Block::STRIPPED_MANGROVE_WOOD.default_state_id,
-            id if id == &Block::OXIDIZED_COPPER.id => Block::WEATHERED_COPPER.default_state_id,
-            id if id == &Block::WEATHERED_COPPER.id => Block::EXPOSED_COPPER.default_state_id,
-            id if id == &Block::EXPOSED_COPPER.id => Block::COPPER_BLOCK.default_state_id,
-            id if id == &Block::WAXED_OXIDIZED_COPPER.id => Block::OXIDIZED_COPPER.default_state_id,
-            id if id == &Block::WAXED_WEATHERED_COPPER.id => {
-                Block::WEATHERED_COPPER.default_state_id
-            }
-            id if id == &Block::WAXED_EXPOSED_COPPER.id => Block::EXPOSED_COPPER.default_state_id,
-            id if id == &Block::WAXED_COPPER_BLOCK.id => Block::COPPER_BLOCK.default_state_id,
-            id if id == &Block::OXIDIZED_CHISELED_COPPER.id => {
-                Block::WEATHERED_CHISELED_COPPER.default_state_id
-            }
-            id if id == &Block::WEATHERED_CHISELED_COPPER.id => {
-                Block::EXPOSED_CHISELED_COPPER.default_state_id
-            }
-            id if id == &Block::EXPOSED_CHISELED_COPPER.id => {
-                Block::CHISELED_COPPER.default_state_id
-            }
-            id if id == &Block::WAXED_OXIDIZED_CHISELED_COPPER.id => {
-                Block::OXIDIZED_CHISELED_COPPER.default_state_id
-            }
-            id if id == &Block::WAXED_WEATHERED_CHISELED_COPPER.id => {
-                Block::WEATHERED_CHISELED_COPPER.default_state_id
-            }
-            id if id == &Block::WAXED_EXPOSED_CHISELED_COPPER.id => {
-                Block::EXPOSED_CHISELED_COPPER.default_state_id
-            }
-            id if id == &Block::WAXED_CHISELED_COPPER.id => Block::CHISELED_COPPER.default_state_id,
-            id if id == &Block::OXIDIZED_COPPER_GRATE.id => {
-                Block::WEATHERED_COPPER_GRATE.default_state_id
-            }
-            id if id == &Block::WEATHERED_COPPER_GRATE.id => {
-                Block::EXPOSED_COPPER_GRATE.default_state_id
-            }
-            id if id == &Block::EXPOSED_COPPER_GRATE.id => Block::COPPER_GRATE.default_state_id,
-            id if id == &Block::WAXED_OXIDIZED_COPPER_GRATE.id => {
-                Block::OXIDIZED_COPPER_GRATE.default_state_id
-            }
-            id if id == &Block::WAXED_WEATHERED_COPPER_GRATE.id => {
-                Block::WEATHERED_COPPER_GRATE.default_state_id
-            }
-            id if id == &Block::WAXED_EXPOSED_COPPER_GRATE.id => {
-                Block::EXPOSED_COPPER_GRATE.default_state_id
-            }
-            id if id == &Block::WAXED_COPPER_GRATE.id => Block::COPPER_GRATE.default_state_id,
-            id if id == &Block::OXIDIZED_CUT_COPPER.id => {
-                Block::WEATHERED_CUT_COPPER.default_state_id
-            }
-            id if id == &Block::WEATHERED_CUT_COPPER.id => {
-                Block::EXPOSED_CUT_COPPER.default_state_id
-            }
-            id if id == &Block::EXPOSED_CUT_COPPER.id => Block::CUT_COPPER.default_state_id,
-            id if id == &Block::WAXED_OXIDIZED_CUT_COPPER.id => {
-                Block::OXIDIZED_CUT_COPPER.default_state_id
-            }
-            id if id == &Block::WAXED_WEATHERED_CUT_COPPER.id => {
-                Block::WEATHERED_CUT_COPPER.default_state_id
-            }
-            id if id == &Block::WAXED_EXPOSED_CUT_COPPER.id => {
-                Block::EXPOSED_CUT_COPPER.default_state_id
-            }
-            id if id == &Block::WAXED_CUT_COPPER.id => Block::CUT_COPPER.default_state_id,
-            id if id == &Block::OXIDIZED_CUT_COPPER_STAIRS.id => {
-                Block::WEATHERED_CUT_COPPER_STAIRS.default_state_id
-            }
-            id if id == &Block::WEATHERED_CUT_COPPER_STAIRS.id => {
-                Block::EXPOSED_CUT_COPPER_STAIRS.default_state_id
-            }
-            id if id == &Block::EXPOSED_CUT_COPPER_STAIRS.id => {
-                Block::CUT_COPPER_STAIRS.default_state_id
-            }
-            id if id == &Block::WAXED_OXIDIZED_CUT_COPPER_STAIRS.id => {
-                Block::OXIDIZED_CUT_COPPER_STAIRS.default_state_id
-            }
-            id if id == &Block::WAXED_WEATHERED_CUT_COPPER_STAIRS.id => {
-                Block::WEATHERED_CUT_COPPER_STAIRS.default_state_id
-            }
-            id if id == &Block::WAXED_EXPOSED_CUT_COPPER_STAIRS.id => {
-                Block::EXPOSED_CUT_COPPER_STAIRS.default_state_id
-            }
-            id if id == &Block::WAXED_CUT_COPPER_STAIRS.id => {
-                Block::CUT_COPPER_STAIRS.default_state_id
-            }
-            id if id == &Block::OXIDIZED_CUT_COPPER_SLAB.id => {
-                Block::WEATHERED_CUT_COPPER_SLAB.default_state_id
-            }
-            id if id == &Block::WEATHERED_CUT_COPPER_SLAB.id => {
-                Block::EXPOSED_CUT_COPPER_SLAB.default_state_id
-            }
-            id if id == &Block::EXPOSED_CUT_COPPER_SLAB.id => {
-                Block::CUT_COPPER_SLAB.default_state_id
-            }
-            id if id == &Block::WAXED_OXIDIZED_CUT_COPPER_SLAB.id => {
-                Block::OXIDIZED_CUT_COPPER_SLAB.default_state_id
-            }
-            id if id == &Block::WAXED_WEATHERED_CUT_COPPER_SLAB.id => {
-                Block::WEATHERED_CUT_COPPER_SLAB.default_state_id
-            }
-            id if id == &Block::WAXED_EXPOSED_CUT_COPPER_SLAB.id => {
-                Block::EXPOSED_CUT_COPPER_SLAB.default_state_id
-            }
-            id if id == &Block::WAXED_CUT_COPPER_SLAB.id => Block::CUT_COPPER_SLAB.default_state_id,
-            id if id == &Block::OXIDIZED_COPPER_BULB.id => {
-                Block::WEATHERED_COPPER_BULB.default_state_id
-            }
-            id if id == &Block::WEATHERED_COPPER_BULB.id => {
-                Block::EXPOSED_COPPER_BULB.default_state_id
-            }
-            id if id == &Block::EXPOSED_COPPER_BULB.id => Block::COPPER_BULB.default_state_id,
-            id if id == &Block::WAXED_OXIDIZED_COPPER_BULB.id => {
-                Block::OXIDIZED_COPPER_BULB.default_state_id
-            }
-            id if id == &Block::WAXED_WEATHERED_COPPER_BULB.id => {
-                Block::WEATHERED_COPPER_BULB.default_state_id
-            }
-            id if id == &Block::WAXED_EXPOSED_COPPER_BULB.id => {
-                Block::EXPOSED_COPPER_BULB.default_state_id
-            }
-            id if id == &Block::WAXED_COPPER_BULB.id => Block::COPPER_BULB.default_state_id,
-            id if id == &Block::OXIDIZED_COPPER_DOOR.id => {
-                Block::WEATHERED_COPPER_DOOR.default_state_id
-            }
-            id if id == &Block::WEATHERED_COPPER_DOOR.id => {
-                Block::EXPOSED_COPPER_DOOR.default_state_id
-            }
-            id if id == &Block::EXPOSED_COPPER_DOOR.id => Block::COPPER_DOOR.default_state_id,
-            id if id == &Block::WAXED_OXIDIZED_COPPER_DOOR.id => {
-                Block::OXIDIZED_COPPER_DOOR.default_state_id
-            }
-            id if id == &Block::WAXED_WEATHERED_COPPER_DOOR.id => {
-                Block::WEATHERED_COPPER_DOOR.default_state_id
-            }
-            id if id == &Block::WAXED_EXPOSED_COPPER_DOOR.id => {
-                Block::EXPOSED_COPPER_DOOR.default_state_id
-            }
-            id if id == &Block::WAXED_COPPER_DOOR.id => Block::COPPER_DOOR.default_state_id,
-            id if id == &Block::OXIDIZED_COPPER_TRAPDOOR.id => {
-                Block::WEATHERED_COPPER_TRAPDOOR.default_state_id
-            }
-            id if id == &Block::WEATHERED_COPPER_TRAPDOOR.id => {
-                Block::EXPOSED_COPPER_TRAPDOOR.default_state_id
-            }
-            id if id == &Block::EXPOSED_COPPER_TRAPDOOR.id => {
-                Block::COPPER_TRAPDOOR.default_state_id
-            }
-            id if id == &Block::WAXED_OXIDIZED_COPPER_TRAPDOOR.id => {
-                Block::OXIDIZED_COPPER_TRAPDOOR.default_state_id
-            }
-            id if id == &Block::WAXED_WEATHERED_COPPER_TRAPDOOR.id => {
-                Block::WEATHERED_COPPER_TRAPDOOR.default_state_id
-            }
-            id if id == &Block::WAXED_EXPOSED_COPPER_TRAPDOOR.id => {
-                Block::EXPOSED_COPPER_TRAPDOOR.default_state_id
-            }
-            id if id == &Block::WAXED_COPPER_TRAPDOOR.id => Block::COPPER_TRAPDOOR.default_state_id,
+        let world = player.world().await;
 
-            _ => block.default_state_id,
-        };
-
-        if block.default_state_id != replacement_block {
-            let world = player.world().await;
-
+        // First we try to strip the block. by getting his equivalent and applying it the axis.
+        let replacement_block = STRIPPED_BLOCKS.get(&block.id);
+        // If there is a strip equivalent.
+        if replacement_block.is_some() {
+            // get block state of the old log.
+            let log_information = world.get_block_state_id(&location).await.unwrap();
+            // get the log properties
+            let log_props = PaleOakWoodLikeProperties::from_state_id(log_information, block);
+            // create new properties for the new log.
+            let mut new_log_properties =
+                PaleOakWoodLikeProperties::default(replacement_block.unwrap());
+            // Set old axis to the new log.
+            new_log_properties.axis = log_props.axis;
             world
-                .set_block_state(&location, replacement_block, BlockFlags::NOTIFY_ALL)
+                .set_block_state(
+                    &location,
+                    new_log_properties.to_state_id(replacement_block.unwrap()),
+                    BlockFlags::NOTIFY_ALL,
+                )
                 .await;
+            return;
         }
+        
     }
 }
