3b44cfea84e0b432cc8270cdbcb4655bc846366a
ioterw <iotrwewe12@protonmail.com>
2025-06-04 04:53:26 +0700

    thx again, clippy

--- pumpkin/src/world/mod.rs
+++ pumpkin/src/world/mod.rs
@@ -1338,15 +1338,12 @@ impl World {
         let (chunk_coordinate, relative) = position.chunk_and_chunk_relative_position();
         let chunk = self.get_chunk(chunk_coordinate).await;
         let mut chunk = chunk.write().await;
-        let replaced_block_state_id = match chunk.section.get_block_absolute_y(
+        let Some(replaced_block_state_id) = chunk.section.get_block_absolute_y(
             relative.x as usize,
             relative.y,
             relative.z as usize,
-        ) {
-            Some(state_id) => state_id,
-            None => {
-                return block_state_id;
-            }
+        ) else {
+            return block_state_id;
         };
 
         if replaced_block_state_id == block_state_id {
