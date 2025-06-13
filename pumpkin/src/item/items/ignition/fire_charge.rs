d298298ce85e76f32d91b3a02d49d4c20ac71541
Destroy <emptycsgo@mail.ru>
2025-06-13 17:36:01 +0300

    changes: added FireChargeItem impl, added ignition logic

---
+++ pumpkin/src/item/items/ignition/fire_charge.rs
@@ -0,0 +1,56 @@
+use async_trait::async_trait;
+use pumpkin_data::Block;
+use pumpkin_data::BlockDirection;
+use pumpkin_data::item::Item;
+use pumpkin_data::sound::{Sound, SoundCategory};
+use pumpkin_util::math::position::BlockPos;
+use pumpkin_world::world::BlockFlags;
+
+use crate::block::blocks::fire::FireBlockBase;
+use crate::entity::player::Player;
+use crate::item::items::ignition::ignition::Ignition;
+use crate::item::pumpkin_item::{ItemMetadata, PumpkinItem};
+use crate::server::Server;
+
+pub struct FireChargeItem;
+
+impl ItemMetadata for FireChargeItem {
+    fn ids() -> Box<[u16]> {
+        [Item::FLINT_AND_STEEL.id].into()
+    }
+}
+
+#[async_trait]
+impl PumpkinItem for FireChargeItem {
+    async fn use_on_block(
+        &self,
+        _item: &Item,
+        player: &Player,
+        location: BlockPos,
+        face: BlockDirection,
+        _block: &Block,
+        _server: &Server,
+    ) {
+        Ignition::ignite_block(
+            async |world, pos| {
+                let fire_block = FireBlockBase::get_fire_type(&world, &pos).await;
+
+                world
+                    .set_block_state(&pos, fire_block.default_state_id, BlockFlags::NOTIFY_ALL)
+                    .await;
+
+                world
+                    .play_block_sound(Sound::BlockFireExtinguish, SoundCategory::Blocks, pos)
+                    .await;
+                // TODO
+            },
+            _item,
+            player,
+            location,
+            face,
+            _block,
+            _server,
+        )
+        .await;
+    }
+}
--- pumpkin/src/item/items/ignition/flint_and_steel.rs
+++ pumpkin/src/item/items/ignition/flint_and_steel.rs
@@ -2,11 +2,13 @@ use async_trait::async_trait;
 use pumpkin_data::Block;
 use pumpkin_data::BlockDirection;
 use pumpkin_data::item::Item;
+use pumpkin_data::sound::{Sound, SoundCategory};
 use pumpkin_util::math::position::BlockPos;
 use pumpkin_world::world::BlockFlags;
 
 use crate::block::blocks::fire::FireBlockBase;
 use crate::entity::player::Player;
+use crate::item::items::ignition::ignition::Ignition;
 use crate::item::pumpkin_item::{ItemMetadata, PumpkinItem};
 use crate::server::Server;
 
@@ -29,16 +31,28 @@ impl PumpkinItem for FlintAndSteelItem {
         _block: &Block,
         _server: &Server,
     ) {
-        // TODO: check CampfireBlock, CandleBlock and CandleCakeBlock
-        let world = player.world().await;
-        let pos = location.offset(face.to_offset());
-        if FireBlockBase::can_place_at(world.as_ref(), &pos).await {
-            let fire_block = FireBlockBase::get_fire_type(&world, &pos).await;
+        Ignition::ignite_block(
+            async |world, pos, block| {
 
-            world
-                .set_block_state(&pos, fire_block.default_state_id, BlockFlags::NOTIFY_ALL)
-                .await;
-            // TODO
-        }
+                let state_block = match block {
+                    Some(block) => block,
+                    None => return,
+                };
+
+                world
+                    .set_block_state(&pos, state_block.default_state_id, BlockFlags::NOTIFY_ALL)
+                    .await;
+
+
+                // TODO
+            },
+            _item,
+            player,
+            location,
+            face,
+            _block,
+            _server,
+        )
+            .await;
     }
 }
