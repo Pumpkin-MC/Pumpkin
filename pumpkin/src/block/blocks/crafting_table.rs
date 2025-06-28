1fd4c33ed9699bb0c682835e66e3dedffc946e43
Liyan Zhao <return65535@qq.com>
2025-06-29 00:28:19 +0800

    fix: add use_with_item method to CraftingTable

--- pumpkin/src/block/blocks/crafting_table.rs
+++ pumpkin/src/block/blocks/crafting_table.rs
@@ -1,9 +1,11 @@
 use crate::block::pumpkin_block::PumpkinBlock;
+use crate::block::registry::BlockActionResult;
 use crate::entity::player::Player;
 use crate::server::Server;
 use crate::world::World;
 use async_trait::async_trait;
 use pumpkin_data::Block;
+use pumpkin_data::item::Item;
 use pumpkin_inventory::crafting::crafting_screen_handler::CraftingTableScreenHandler;
 use pumpkin_inventory::player::player_inventory::PlayerInventory;
 use pumpkin_inventory::screen_handler::{InventoryPlayer, ScreenHandler, ScreenHandlerFactory};
@@ -29,6 +31,21 @@ impl PumpkinBlock for CraftingTableBlock {
         player
             .open_handled_screen(&CraftingTableScreenFactory)
             .await;
+    }
+
+    async fn use_with_item(
+        &self,
+        _block: &Block,
+        player: &Player,
+        _location: BlockPos,
+        _item: &Item,
+        _server: &Server,
+        _world: &Arc<World>,
+    ) -> BlockActionResult {
+        player
+            .open_handled_screen(&CraftingTableScreenFactory)
+            .await;
+        BlockActionResult::Consume
     }
 }
 
