5d2abc98bfa566b0425f31385c9b18f37868f5a7
tn-lorenz <lorenz.tim-niklas@protonmail.com>
2025-06-30 20:46:34 +0200

    CLIPPY

--- pumpkin/src/entity/player.rs
+++ pumpkin/src/entity/player.rs
@@ -19,7 +19,7 @@ use uuid::Uuid;
 use pumpkin_config::{BASIC_CONFIG, advanced_config};
 use pumpkin_data::damage::DamageType;
 use pumpkin_data::entity::{EffectType, EntityPose, EntityStatus, EntityType};
-use pumpkin_data::item::{Item, Operation};
+use pumpkin_data::item::Operation;
 use pumpkin_data::particle::Particle;
 use pumpkin_data::sound::{Sound, SoundCategory};
 use pumpkin_data::tag::Tagable;
