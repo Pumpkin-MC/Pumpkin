834a1ad3849f05f53d9d0639ad47674ec93f5526
tom-devv <73348376+tom-devv@users.noreply.github.com>
2025-07-03 12:50:03 +0100

    refactor to struct args

--- pumpkin/src/block/blocks/anvil.rs
+++ pumpkin/src/block/blocks/anvil.rs
@@ -1,13 +1,7 @@
-use crate::block::BlockIsReplacing;
-use crate::block::pumpkin_block::{BlockMetadata, PumpkinBlock};
-use crate::server::Server;
-use crate::{entity::player::Player, world::World};
+use crate::block::pumpkin_block::{BlockMetadata, OnPlaceArgs, PumpkinBlock};
 use async_trait::async_trait;
 use pumpkin_data::block_properties::{BlockProperties, WallTorchLikeProperties};
 use pumpkin_data::tag::{RegistryKey, get_tag_values};
-use pumpkin_data::{Block, BlockDirection};
-use pumpkin_protocol::java::server::play::SUseItemOn;
-use pumpkin_util::math::position::BlockPos;
 use pumpkin_world::BlockStateId;
 
 pub struct AnvilBlock;
@@ -24,26 +18,17 @@ impl BlockMetadata for AnvilBlock {
 
 #[async_trait]
 impl PumpkinBlock for AnvilBlock {
-    async fn on_place(
-        &self,
-        _server: &Server,
-        _world: &World,
-        player: &Player,
-        block: &Block,
-        _block_pos: &BlockPos,
-        _face: BlockDirection,
-        _replacing: BlockIsReplacing,
-        _use_item_on: &SUseItemOn,
-    ) -> BlockStateId {
-        let dir = player
+    async fn on_place(&self, args: OnPlaceArgs<'_>) -> BlockStateId {
+        let dir = args
+            .player
             .living_entity
             .entity
             .get_horizontal_facing()
             .rotate_clockwise();
 
-        let mut props = WallTorchLikeProperties::default(block);
+        let mut props = WallTorchLikeProperties::default(args.block);
 
         props.facing = dir;
-        props.to_state_id(block)
+        props.to_state_id(args.block)
     }
 }
