085be53c63650fb4082b3a8baaf56bc5675ddb77
Ancient77 <76914086+Ancient77@users.noreply.github.com>
2025-07-06 14:03:25 +0200

    add sign edit behavior

--- pumpkin/src/block/blocks/signs.rs
+++ pumpkin/src/block/blocks/signs.rs
@@ -2,17 +2,16 @@ use std::sync::Arc;
 
 use async_trait::async_trait;
 use pumpkin_data::block_properties::BlockProperties;
-use pumpkin_data::block_properties::EnumVariants;
-use pumpkin_data::block_properties::Integer0To15;
 use pumpkin_data::tag::RegistryKey;
 use pumpkin_data::tag::get_tag_values;
 use pumpkin_world::block::entities::sign::SignBlockEntity;
 
-use crate::block::pumpkin_block::OnPlaceArgs;
+use crate::block::pumpkin_block::NormalUseArgs;
 use crate::block::pumpkin_block::OnStateReplacedArgs;
 use crate::block::pumpkin_block::PlacedArgs;
 use crate::block::pumpkin_block::PlayerPlacedArgs;
-use crate::block::pumpkin_block::{BlockMetadata, PumpkinBlock};
+use crate::block::pumpkin_block::{BlockMetadata, OnPlaceArgs, PumpkinBlock};
+use crate::block::registry::BlockActionResult;
 use crate::entity::EntityBase;
 
 type SignProperties = pumpkin_data::block_properties::OakSignLikeProperties;
@@ -53,5 +52,27 @@ impl PumpkinBlock for SignBlock {
 
     async fn on_state_replaced(&self, args: OnStateReplacedArgs<'_>) {
         args.world.remove_block_entity(args.position).await;
+    }
+
+    async fn normal_use(&self, args: NormalUseArgs<'_>) -> BlockActionResult {
+        if let Some(block_entity) = args.world.get_block_entity(args.position).await {
+            if let Some(sign_entity) = block_entity.as_any().downcast_ref::<SignBlockEntity>() {
+                if sign_entity.is_waxed {
+                    args.world
+                        .play_block_sound(
+                            pumpkin_data::sound::Sound::BlockSignWaxedInteractFail,
+                            pumpkin_data::sound::SoundCategory::Blocks,
+                            *args.position,
+                        )
+                        .await;
+                } else {
+                    match &args.player.client {
+                        crate::net::ClientPlatform::Java(java) => java.send_sign_packet(*args.position).await,
+                        crate::net::ClientPlatform::Bedrock(_bedrock) => todo!(),
+                    };
+                }
+            }
+        }
+        BlockActionResult::Continue
     }
 }
