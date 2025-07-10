843470bfae079d6a7069dac7b0f1c4347e543855
unschlagbar <153451111+unschlagbar@users.noreply.github.com>
2025-07-10 22:36:30 +0200

    dyv

--- pumpkin/src/world/mod.rs
+++ pumpkin/src/world/mod.rs
@@ -53,14 +53,19 @@ use pumpkin_inventory::{equipment_slot::EquipmentSlot, screen_handler::Inventory
 use pumpkin_macros::send_cancellable;
 use pumpkin_nbt::{compound::NbtCompound, to_bytes_unnamed};
 use pumpkin_protocol::{
+    ClientPacket, IdOr, SoundEvent,
     bedrock::{
+        RakReliability,
         client::{
             gamerules_changed::GameRules,
             start_game::{Experiments, GamePublishSetting, LevelSettings},
-        }, RakReliability
-    }, codec::{
-        bedrock_block_pos::BedrockPos, little_endian::Le32, var_long::VarLong, var_uint::VarUInt, var_ulong::VarULong
-    }, java::{
+        },
+    },
+    codec::{
+        bedrock_block_pos::BedrockPos, little_endian::Le32, var_long::VarLong, var_uint::VarUInt,
+        var_ulong::VarULong,
+    },
+    java::{
         client::play::{
             CBlockEntityData, CEntityStatus, CGameEvent, CLogin, CMultiBlockUpdate,
             CPlayerChatMessage, CPlayerInfoUpdate, CRemoveEntities, CRemovePlayerInfo,
@@ -68,7 +73,7 @@ use pumpkin_protocol::{
             PlayerAction, PlayerInfoFlags,
         },
         server::play::SChatMessage,
-    }, ClientPacket, IdOr, SoundEvent
+    },
 };
 use pumpkin_protocol::{bedrock::client::start_game::CStartGame, ser::serializer::Serializer};
 use pumpkin_protocol::{
