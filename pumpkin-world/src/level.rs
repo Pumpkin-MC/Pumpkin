28b87f721b823de680f12ce43d2e1eb887313577
4lve <72332750+4lve@users.noreply.github.com>
2025-07-11 14:22:07 +0200

    Remove unused imports

--- pumpkin-world/src/level.rs
+++ pumpkin-world/src/level.rs
@@ -405,7 +405,6 @@ impl Level {
         };
         let mut rng = SmallRng::from_os_rng();
         for chunk in self.loaded_chunks.iter() {
-            use tokio::time::{Duration, timeout};
             let mut chunk = chunk.write().await;
             ticks.block_ticks.extend(chunk.get_and_tick_block_ticks());
             ticks.fluid_ticks.extend(chunk.get_and_tick_fluid_ticks());
