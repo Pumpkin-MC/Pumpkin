b631aa3870b7b15448c2c29ff220a1724064e0e3
Liyan Zhao <return65535@qq.com>
2025-07-30 01:48:53 +0800

    ci

--- pumpkin/src/logging.rs
+++ pumpkin/src/logging.rs
@@ -131,7 +131,7 @@ impl Log for GzipRollingLogger {
         metadata.level() <= self.log_level
     }
 
-    fn log<'a>(&self, record: &'a log::Record) {
+    fn log<'a, 'b:'a>(&self, record: &'a log::Record) {
         if !self.enabled(record.metadata()) {
             return;
         }
@@ -141,7 +141,7 @@ impl Log for GzipRollingLogger {
         if let Ok(data) = self.data.lock() {
             let original_string = format(record.args().clone());
             let string = remove_ansi_color_code(&original_string);
-            let record: Record = Record::builder()
+            let record: Record<'b> = Record::<'b>::builder()
                 .args(format_args!("{}", string))
                 .metadata(record.metadata().clone())
                 .module_path(record.module_path())
