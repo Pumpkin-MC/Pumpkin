8cb1455676d6373a399e75b90e0c99161d82c23f
Liyan Zhao <return65535@qq.com>
2025-08-04 21:35:24 +0800

    fix

--- pumpkin/src/entity/mob/mod.rs
+++ pumpkin/src/entity/mob/mod.rs
@@ -11,7 +11,6 @@ use pumpkin_data::damage::DamageType;
 use pumpkin_data::entity::EntityType;
 use pumpkin_util::math::position::BlockPos;
 use pumpkin_util::math::vector3::Vector3;
-use std::ptr::addr_eq;
 use std::sync::Arc;
 use std::sync::atomic::AtomicI32;
 use std::sync::atomic::Ordering::Relaxed;
@@ -63,48 +62,47 @@ impl MobEntity {
     }
 
     fn get_base_mob_experience_drop(entity_type: &EntityType) -> u32 {
-        if addr_eq(entity_type, &EntityType::SLIME) {
+        if entity_type.id == EntityType::SLIME.id {
             1 // Slimes drop 1-3 experience depending on size
-        } else if addr_eq(entity_type, &EntityType::ENDERMITE)
-            || addr_eq(entity_type, &EntityType::VEX)
+        } else if entity_type.id == EntityType::ENDERMITE.id || entity_type.id == EntityType::VEX.id
         {
             3
-        } else if addr_eq(entity_type, &EntityType::GHAST)
-            || addr_eq(entity_type, &EntityType::PIGLIN)
-            || addr_eq(entity_type, &EntityType::PHANTOM)
-            || addr_eq(entity_type, &EntityType::WARDEN)
-            || addr_eq(entity_type, &EntityType::ILLUSIONER)
-            || addr_eq(entity_type, &EntityType::ZOMBIE)
-            || addr_eq(entity_type, &EntityType::ZOGLIN)
-            || addr_eq(entity_type, &EntityType::SHULKER)
-            || addr_eq(entity_type, &EntityType::HOGLIN)
-            || addr_eq(entity_type, &EntityType::ZOMBIFIED_PIGLIN)
-            || addr_eq(entity_type, &EntityType::ZOMBIE_VILLAGER)
-            || addr_eq(entity_type, &EntityType::WITHER_SKELETON)
-            || addr_eq(entity_type, &EntityType::WITCH)
-            || addr_eq(entity_type, &EntityType::VINDICATOR)
-            || addr_eq(entity_type, &EntityType::STRAY)
-            || addr_eq(entity_type, &EntityType::SPIDER)
-            || addr_eq(entity_type, &EntityType::SKELETON)
-            || addr_eq(entity_type, &EntityType::PILLAGER)
-            || addr_eq(entity_type, &EntityType::HUSK)
-            || addr_eq(entity_type, &EntityType::EVOKER)
-            || addr_eq(entity_type, &EntityType::ENDERMAN)
-            || addr_eq(entity_type, &EntityType::DROWNED)
-            || addr_eq(entity_type, &EntityType::CREEPER)
-            || addr_eq(entity_type, &EntityType::CAVE_SPIDER)
-            || addr_eq(entity_type, &EntityType::BOGGED)
+        } else if entity_type.id == EntityType::GHAST.id
+            || entity_type.id == EntityType::PIGLIN.id
+            || entity_type.id == EntityType::PHANTOM.id
+            || entity_type.id == EntityType::WARDEN.id
+            || entity_type.id == EntityType::ILLUSIONER.id
+            || entity_type.id == EntityType::ZOMBIE.id
+            || entity_type.id == EntityType::ZOGLIN.id
+            || entity_type.id == EntityType::SHULKER.id
+            || entity_type.id == EntityType::HOGLIN.id
+            || entity_type.id == EntityType::ZOMBIFIED_PIGLIN.id
+            || entity_type.id == EntityType::ZOMBIE_VILLAGER.id
+            || entity_type.id == EntityType::WITHER_SKELETON.id
+            || entity_type.id == EntityType::WITCH.id
+            || entity_type.id == EntityType::VINDICATOR.id
+            || entity_type.id == EntityType::STRAY.id
+            || entity_type.id == EntityType::SPIDER.id
+            || entity_type.id == EntityType::SKELETON.id
+            || entity_type.id == EntityType::PILLAGER.id
+            || entity_type.id == EntityType::HUSK.id
+            || entity_type.id == EntityType::EVOKER.id
+            || entity_type.id == EntityType::ENDERMAN.id
+            || entity_type.id == EntityType::DROWNED.id
+            || entity_type.id == EntityType::CREEPER.id
+            || entity_type.id == EntityType::CAVE_SPIDER.id
+            || entity_type.id == EntityType::BOGGED.id
         {
             5
-        } else if addr_eq(entity_type, &EntityType::EVOKER)
-            || addr_eq(entity_type, &EntityType::BREEZE)
-            || addr_eq(entity_type, &EntityType::GUARDIAN)
-            || addr_eq(entity_type, &EntityType::ELDER_GUARDIAN)
-            || addr_eq(entity_type, &EntityType::BLAZE)
+        } else if entity_type.id == EntityType::EVOKER.id
+            || entity_type.id == EntityType::BREEZE.id
+            || entity_type.id == EntityType::GUARDIAN.id
+            || entity_type.id == EntityType::ELDER_GUARDIAN.id
+            || entity_type.id == EntityType::BLAZE.id
         {
             10
-        } else if addr_eq(entity_type, &EntityType::PIGLIN_BRUTE)
-            || addr_eq(entity_type, &EntityType::RAVAGER)
+        } else if entity_type.id == EntityType::PIGLIN_BRUTE.id
+            || entity_type.id == EntityType::RAVAGER.id
         {
             20
         } else {
