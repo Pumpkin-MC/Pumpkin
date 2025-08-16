19152e4631a67880e0e1f2840378e4dabc7914a9
FJKiXfaR <073224131@szut.edu.cn>
2025-08-16 17:19:51 +0800

    Fix code specifications

--- pumpkin-config/src/networking/networks.rs
+++ pumpkin-config/src/networking/networks.rs
@@ -1,15 +1,7 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Default)]
 pub struct NetworksConfig {
     /// Enable IPv6 support with dual-stack binding (falls back to IPv4 if unavailable)
     pub ipv6_enabled: bool,
-}
-
-impl Default for NetworksConfig {
-    fn default() -> Self {
-        Self {
-            ipv6_enabled: false,
-        }
-    }
 }
