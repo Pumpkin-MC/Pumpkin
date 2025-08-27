3fda7fff17764d3d7f11da601dcbce8c419aba5c
Joniii11 <88550325+Joniii11@users.noreply.github.com>
2025-08-27 23:06:08 +0200

    feat: Add checking for if space is occupied of the armor stand

--- pumpkin/src/item/items/armor_stand.rs
+++ pumpkin/src/item/items/armor_stand.rs
@@ -10,6 +10,7 @@ use pumpkin_data::entity::EntityType;
 use pumpkin_data::item::Item;
 use pumpkin_data::sound::{Sound, SoundCategory};
 use pumpkin_data::{Block, BlockDirection};
+use pumpkin_util::math::boundingbox::BoundingBox;
 use pumpkin_util::math::position::BlockPos;
 use pumpkin_util::math::vector3::Vector3;
 use pumpkin_util::math::wrap_degrees;
@@ -51,25 +52,49 @@ impl ItemBehaviour for ArmorStandItem {
         let world = player.world();
         let position = Self::calculate_placement_position(&location, &face).to_f64();
 
-        let (player_yaw, _) = player.rotation();
-        let rotation = ((wrap_degrees(player_yaw - 180.0) + 22.5) / 45.0).floor() * 45.0;
-
-        let entity = Entity::new(
-            Uuid::new_v4(),
-            world.clone(),
-            position,
-            &EntityType::ARMOR_STAND,
-            false
+        let bottom_center = Vector3::new(
+            position.x,
+            position.y,
+            position.z
         );
 
-        // TODO: There's a problem with the entity's rotation at East -90. Other people can also replicate it
-        entity.set_rotation(rotation, 0.0);
+        let armor_stand_dimensions = EntityType::ARMOR_STAND.dimension;
+        let width = armor_stand_dimensions[0] as f64;
+        let height = armor_stand_dimensions[1] as f64;
 
-        world.play_sound(Sound::EntityArmorStandPlace, SoundCategory::Blocks, &entity.pos.load()).await;
+        let bounding_box = BoundingBox::new(
+            Vector3::new(
+                bottom_center.x - width / 2.0,
+                bottom_center.y,
+                bottom_center.z - width / 2.0
+            ),
+            Vector3::new(
+                bottom_center.x + width / 2.0,
+                bottom_center.y + height,
+                bottom_center.z + width / 2.0
+            )
+        );
 
-        let armor_stand = ArmorStandEntity::new(entity);
+        if world.is_space_empty(bounding_box.clone()).await && world.get_entities_at_box(&bounding_box).await.is_empty() {
+            let (player_yaw, _) = player.rotation();
+            let rotation = ((wrap_degrees(player_yaw - 180.0) + 22.5) / 45.0).floor() * 45.0;
 
-        world.spawn_entity(Arc::new(armor_stand)).await;
+            let entity = Entity::new(
+                Uuid::new_v4(),
+                world.clone(),
+                position,
+                &EntityType::ARMOR_STAND,
+                false
+            );
+
+            entity.set_rotation(rotation, 0.0);
+
+            world.play_sound(Sound::EntityArmorStandPlace, SoundCategory::Blocks, &entity.pos.load()).await;
+
+            let armor_stand = ArmorStandEntity::new(entity);
+
+            world.spawn_entity(Arc::new(armor_stand)).await;
+        }
     }
 
     fn as_any(&self) -> &dyn std::any::Any {
