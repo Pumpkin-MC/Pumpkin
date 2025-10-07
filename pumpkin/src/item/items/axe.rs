9b1c075a7e264a23cef29ce568117e5d986f4e42
JunkyDeveloper <junkydeveloper@gmail.com>
2025-10-07 15:11:51 +0200

    deoxidizing was removing the hanging property. It stays in the state now

--- pumpkin/src/item/items/axe.rs
+++ pumpkin/src/item/items/axe.rs
@@ -4,7 +4,9 @@ use crate::server::Server;
 use async_trait::async_trait;
 use pumpkin_data::BlockDirection;
 use pumpkin_data::block_properties::BlockProperties;
-use pumpkin_data::block_properties::{OakDoorLikeProperties, PaleOakWoodLikeProperties, OakFenceLikeProperties};
+use pumpkin_data::block_properties::{
+    LanternLikeProperties, OakDoorLikeProperties, OakFenceLikeProperties, PaleOakWoodLikeProperties,
+};
 use pumpkin_data::tag::Taggable;
 use pumpkin_data::{Block, tag};
 use pumpkin_util::math::position::BlockPos;
@@ -67,7 +69,7 @@ impl ItemBehaviour for AxeItem {
                 new_door_properties.hinge = door_props.hinge;
                 new_door_properties.powered = door_props.powered;
                 new_door_properties.to_state_id(new_block)
-            } else if block.is_tagged_with_by_tag(&tag::Block::MINECRAFT_BARS){
+            } else if block.is_tagged_with_by_tag(&tag::Block::MINECRAFT_BARS) {
                 let bar_information = world.get_block_state_id(&location).await;
                 let bar_props = OakFenceLikeProperties::from_state_id(bar_information, block);
                 let mut new_bars_props = OakFenceLikeProperties::default(new_block);
@@ -76,6 +78,13 @@ impl ItemBehaviour for AxeItem {
                 new_bars_props.west = bar_props.west;
                 new_bars_props.east = bar_props.east;
                 new_bars_props.to_state_id(new_block)
+            } else if block.is_tagged_with_by_tag(&tag::Block::MINECRAFT_LANTERNS) {
+                let lantern_information = world.get_block_state_id(&location).await;
+                let lantern_props =
+                    LanternLikeProperties::from_state_id(lantern_information, block);
+                let mut new_lantern_props = LanternLikeProperties::default(new_block);
+                new_lantern_props.hanging = lantern_props.hanging;
+                new_lantern_props.to_state_id(new_block)
             } else {
                 new_block.default_state.id
             };
