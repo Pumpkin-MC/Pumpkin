48d7932e5f4bb044f3e0a67b7e165ae758e5d1f5
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2026-01-13 21:03:54 +1100

    More consistent formatting

--- pumpkin-inventory/src/player/player_inventory.rs
+++ pumpkin-inventory/src/player/player_inventory.rs
@@ -300,7 +300,12 @@ impl PlayerInventory {
                 player
                     .enqueue_slot_set_packet(&CSetPlayerInventory::new(
                         (room_for_stack as i32).into(),
-                        &self.get_stack(room_for_stack as usize).await.lock().await.clone().into()
+                        &self.get_stack(room_for_stack as usize)
+                            .await
+                            .lock()
+                            .await
+                            .clone()
+                            .into()
                     ))
                     .await;
             }
