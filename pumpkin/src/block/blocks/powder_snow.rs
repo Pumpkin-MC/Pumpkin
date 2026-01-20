1d34950a97e3f07ede827cf1809eb04ff63b02d4
Felipe Muñoz Mazur <felipe.munoz.mazur@gmail.com>
2026-01-20 12:43:59 -0300

    feat: add Powder Snow and Slime blocks with fall damage handling

---
+++ pumpkin/src/block/blocks/powder_snow.rs
@@ -0,0 +1,23 @@
+use pumpkin_data::sound::Sound;
+use pumpkin_macros::pumpkin_block;
+
+use crate::block::{BlockBehaviour, BlockFuture, OnLandedUponArgs};
+
+#[pumpkin_block("minecraft:powder_snow")]
+pub struct PowderSnowBlock;
+
+impl BlockBehaviour for PowderSnowBlock {
+    fn on_landed_upon<'a>(&'a self, args: OnLandedUponArgs<'a>) -> BlockFuture<'a, ()> {
+        Box::pin(async move {
+            if let Some(living) = args.entity.get_living_entity() && !(args.fall_distance < 4.0) {
+                let sound = if args.fall_distance < 7.0 {
+                    Sound::EntityGenericSmallFall
+                } else {
+                    Sound::EntityGenericBigFall
+                };
+
+                living.entity.play_sound(sound).await;
+            }
+        })
+    }
+}
