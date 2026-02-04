1302abdc02bb609ac1cb138990c5df9962742fc0
yunuservices <yunuservices@gmail.com>
2026-02-05 00:54:02 +0300

    Add player resource pack status and respawn events

--- pumpkin/src/net/java/config.rs
+++ pumpkin/src/net/java/config.rs
@@ -6,6 +6,7 @@ use crate::{
         PlayerConfig, can_not_join,
         java::{JavaClient, PacketHandlerResult},
     },
+    plugin::player::player_resource_pack_status::PlayerResourcePackStatusEvent,
     server::Server,
 };
 use core::str;
@@ -81,21 +82,24 @@ impl JavaClient {
                 uuid::Uuid::new_v3(&uuid::Uuid::NAMESPACE_DNS, resource_config.url.as_bytes());
 
             if packet.uuid == expected_uuid {
-                match packet.response_result() {
+                let status = match packet.response_result() {
                     ResourcePackResponseResult::DownloadSuccess => {
                         log::trace!(
                             "Client {} successfully downloaded the resource pack",
                             self.id
                         );
+                        "SUCCESSFULLY_LOADED"
                     }
                     ResourcePackResponseResult::DownloadFail => {
                         log::warn!(
                             "Client {} failed to downloaded the resource pack. Is it available on the internet?",
                             self.id
                         );
+                        "FAILED_DOWNLOAD"
                     }
                     ResourcePackResponseResult::Downloaded => {
                         log::trace!("Client {} already has the resource pack", self.id);
+                        "DOWNLOADED"
                     }
                     ResourcePackResponseResult::Accepted => {
                         log::trace!("Client {} accepted the resource pack", self.id);
@@ -105,18 +109,22 @@ impl JavaClient {
                     }
                     ResourcePackResponseResult::Declined => {
                         log::trace!("Client {} declined the resource pack", self.id);
+                        "DECLINED"
                     }
                     ResourcePackResponseResult::InvalidUrl => {
                         log::warn!(
                             "Client {} reported that the resource pack URL is invalid!",
                             self.id
                         );
+                        "INVALID_URL"
                     }
                     ResourcePackResponseResult::ReloadFailed => {
                         log::trace!("Client {} failed to reload the resource pack", self.id);
+                        "FAILED_RELOAD"
                     }
                     ResourcePackResponseResult::Discarded => {
                         log::trace!("Client {} discarded the resource pack", self.id);
+                        "DISCARDED"
                     }
                     ResourcePackResponseResult::Unknown(result) => {
                         log::warn!(
@@ -124,6 +132,19 @@ impl JavaClient {
                             self.id,
                             result
                         );
+                        "FAILED_DOWNLOAD"
+                    }
+                };
+
+                if let Some(profile) = self.gameprofile.lock().await.clone() {
+                    if let Some(player) = server.get_player_by_uuid(profile.id) {
+                        let event = PlayerResourcePackStatusEvent::new(
+                            player,
+                            packet.uuid,
+                            resource_config.sha1.clone(),
+                            status.to_string(),
+                        );
+                        server.plugin_manager.fire(event).await;
                     }
                 }
             } else {
