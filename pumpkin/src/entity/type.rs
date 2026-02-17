a66d59b776b6efc59b08a57ba9c5fcdac04092e3
Purdze <r.s.sutton@hotmail.co.uk>
2026-02-17 19:13:54 +0000

    pig ai

--- pumpkin/src/entity/type.rs
+++ pumpkin/src/entity/type.rs
@@ -26,6 +26,7 @@ use crate::{
         },
         passive::{
             cat::CatEntity, chicken::ChickenEntity, cow::CowEntity, iron_golem::IronGolemEntity,
+            pig::PigEntity,
             sheep::SheepEntity, snow_golem::SnowGolemEntity, wolf::WolfEntity,
         },
     },
@@ -61,6 +62,7 @@ pub async fn from_type(
         id if id == EntityType::CAT.id => CatEntity::new(entity).await,
         id if id == EntityType::CHICKEN.id => ChickenEntity::new(entity).await,
         id if id == EntityType::COW.id => CowEntity::new(entity).await,
+        id if id == EntityType::PIG.id => PigEntity::new(entity).await,
         id if id == EntityType::SNOW_GOLEM.id => SnowGolemEntity::new(entity).await,
         id if id == EntityType::IRON_GOLEM.id => IronGolemEntity::new(entity).await,
         id if id == EntityType::SHEEP.id => SheepEntity::new(entity).await,
