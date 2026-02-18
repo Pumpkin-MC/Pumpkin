b4bfc1588f9322573835efb96028b93f355e61e7
Purdze <r.s.sutton@hotmail.co.uk>
2026-02-18 17:02:33 +0000

    feat: cat ai with escape panic, tempt, and wandering

--- pumpkin/src/entity/type.rs
+++ pumpkin/src/entity/type.rs
@@ -25,7 +25,8 @@ use crate::{
             zombie_villager::ZombieVillagerEntity,
         },
         passive::{
-            iron_golem::IronGolemEntity, sheep::SheepEntity, snow_golem::SnowGolemEntity,
+            cat::CatEntity, iron_golem::IronGolemEntity, sheep::SheepEntity,
+            snow_golem::SnowGolemEntity,
             wolf::WolfEntity,
         },
     },
@@ -58,6 +59,7 @@ pub async fn from_type(
         id if id == EntityType::CREEPER.id => CreeperEntity::new(entity).await,
         id if id == EntityType::ENDERMAN.id => EndermanEntity::new(entity).await,
 
+        id if id == EntityType::CAT.id => CatEntity::new(entity).await,
         id if id == EntityType::SNOW_GOLEM.id => SnowGolemEntity::new(entity).await,
         id if id == EntityType::IRON_GOLEM.id => IronGolemEntity::new(entity).await,
         id if id == EntityType::SHEEP.id => SheepEntity::new(entity).await,
