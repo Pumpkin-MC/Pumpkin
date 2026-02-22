6f02947b3d38d6f1fe2adb1d3a3bacdec9f310e9
yunuservices <yunuservices@gmail.com>
2026-02-22 15:19:40 +0300

    style: apply rustfmt

--- pumpkin/src/plugin/api/events/player/mod.rs
+++ pumpkin/src/plugin/api/events/player/mod.rs
@@ -1,6 +1,7 @@
 pub mod changed_main_hand;
 pub mod exp_change;
 pub mod fish;
+pub mod item_held;
 pub mod player_change_world;
 pub mod player_chat;
 pub mod player_command_send;
@@ -9,7 +10,6 @@ pub mod player_gamemode_change;
 pub mod player_interact_entity_event;
 pub mod player_interact_event;
 pub mod player_interact_unknown_entity_event;
-pub mod item_held;
 pub mod player_join;
 pub mod player_leave;
 pub mod player_login;
