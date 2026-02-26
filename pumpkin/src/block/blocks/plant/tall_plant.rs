41a71307135199ceda893c800d302f8161cdcce3
Joseph Rimmer <4097668+yuminstalljoe@users.noreply.github.com>
2026-02-26 04:32:29 -0500

    fix: allow tall plants to place their upper half (#949)

--- pumpkin/src/block/blocks/plant/tall_plant.rs
+++ pumpkin/src/block/blocks/plant/tall_plant.rs
@@ -1,8 +1,11 @@
+use crate::block::PlacedArgs;
 use pumpkin_data::Block;
+use pumpkin_data::BlockDirection;
 use pumpkin_data::block_properties::{
     BlockProperties, DoubleBlockHalf, TallSeagrassLikeProperties,
 };
 use pumpkin_world::BlockStateId;
+use pumpkin_world::world::BlockFlags;
 
 use crate::block::BlockFuture;
 use crate::block::{
@@ -65,6 +68,20 @@ impl BlockBehaviour for TallPlantBlock {
                 }
             }
             Block::AIR.default_state.id
+        })
+    }
+    fn placed<'a>(&'a self, args: PlacedArgs<'a>) -> BlockFuture<'a, ()> {
+        Box::pin(async move {
+            let mut tall_plant_props =
+                TallSeagrassLikeProperties::from_state_id(args.state_id, args.block);
+            tall_plant_props.half = DoubleBlockHalf::Upper;
+            args.world
+                .set_block_state(
+                    &args.position.offset(BlockDirection::Up.to_offset()),
+                    tall_plant_props.to_state_id(args.block),
+                    BlockFlags::NOTIFY_ALL | BlockFlags::SKIP_BLOCK_ADDED_CALLBACK,
+                )
+                .await;
         })
     }
 }
