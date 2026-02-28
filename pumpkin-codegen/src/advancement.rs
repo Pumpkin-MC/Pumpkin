433c5d54123bd85d1fbff1efc03aa80dffb6c6f0
kedor <99351777+aure31@users.noreply.github.com>
2026-02-28 11:50:50 +0100

    advancing the implementation of the advancements (can build now)

--- pumpkin-codegen/src/advancement.rs
+++ pumpkin-codegen/src/advancement.rs
@@ -6,30 +6,17 @@ use pumpkin_util::text::TextComponent;
 use quote::{format_ident, quote, ToTokens};
 use serde::Deserialize;
 use pumpkin_util::resource_location::ResourceLocation;
+use pumpkin_util::text::TextContent::Translate;
 
-#[derive(Deserialize)]
+#[derive(Deserialize,Default)]
 pub struct Advancement {
     pub parent : Option<ResourceLocation>,
     // pub display : Option<AdvancementDisplay>,
     // pub criteria : Vec<AdvancementCriterion>,
     // pub rewards : AdvancementRewards,
+    #[serde(default)]
     pub send_telemetry : bool,
-    pub name : Option<TextComponent>
-}
-
-impl Advancement {
-    fn to_tokens(&self) {
-        let parent = match &self.parent {
-            Some(parent) => quote! { Some(#parent) },
-            None => quote! { None }
-        };
-        let send_telemetry = self.send_telemetry;
-        let name = match &self.name {
-            Some(name) => quote! { #name },
-            None => quote! { None }
-        };
-
-    }
+    pub display_name : Option<TextComponent>
 }
 
 pub(crate) fn build() -> TokenStream {
@@ -39,19 +26,64 @@ pub(crate) fn build() -> TokenStream {
         serde_json::from_str(&fs::read_to_string("../assets/advancements.json").unwrap())
             .expect("Failed to parse advancements.json");
 
-    for (name, advancement) in advancements {
-        let raw_name = name.strip_prefix("minecraft:").unwrap();
+    let mut variants = TokenStream::new();
+    let mut name_to_type = TokenStream::new();
+    let mut minecraft_name_to_type = TokenStream::new();
+
+    for (minecraft_name, advancement) in advancements {
+        let raw_name = minecraft_name.strip_prefix("minecraft:").unwrap();
         let format_name = format_ident!("{}", raw_name.to_shouty_snake_case());
 
+        let parent = match &advancement.parent {
+            Some(parent) => quote! { Some(#parent) },
+            None => quote! { None }
+        };
+        let send_telemetry = advancement.send_telemetry;
+        let display_name = match &advancement.display_name {
+            Some(name) => {
+                let Translate { translate, with: _ } = &name.0.content else { panic!() };
+                quote! { TextComponent::translate(#translate,[]) }
+            }
+            None => quote! { None }
+        };
+        variants.extend([quote! {
+            pub const #format_name: Self = Self {
+                id: #raw_name,
+                parent : #parent,
+                send_telemetry : #send_telemetry,
+                display_name : #display_name,
+            };
+        }]);
+        name_to_type.extend(quote! { #raw_name => Some(&Self::#format_name), });
+        minecraft_name_to_type.extend(quote! { #minecraft_name => Some(&Self::#format_name), });
     }
 
+
+
     quote! {
-        pub struct Advancements {
+        pub struct Advancement {
+            pub id : ResourceLocation,
             pub parent : Option<ResourceLocation>,
             pub send_telemetry : bool,
-            pub name : Option<TextComponent>
+            pub display_name : Option<TextComponent>
         }
 
+        impl Advancement {
+            #variants
 
+            pub fn from_name(name: &str) -> Option<&'static Self> {
+                    match name {
+                        #name_to_type
+                        _ => None
+                    }
+                }
+
+            pub fn from_minecraft_name(name: &str) -> Option<&'static Self> {
+                match name {
+                    #name_to_type
+                    _ => None
+                }
+            }
+        }
     }
 }
