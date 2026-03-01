db401887cdb02d938a3fa687207d4f8bb527dddb
SomeYellowGuy <30015613+SomeYellowGuy@users.noreply.github.com>
2026-03-01 17:43:42 +0530

    fixed a few more issues

--- pumpkin-codecs/src/data_result.rs
+++ pumpkin-codecs/src/data_result.rs
@@ -556,12 +556,11 @@ impl<R> DataResult<R> {
                 Self::Success { result, .. },
                 DataResult::Error {
                     message: m2,
-                    partial_result,
                     ..
                 },
             ) => Self::new_option_error_with_lifecycle(
                 m2.clone(),
-                partial_result.is_some().then_some(result),
+                Some(result),
                 Lifecycle::Stable,
             ),
         }
