45d3e24035f9f185ac556ea67fb09ad689ecff86
Bjorn Beishline <75190918+BjornTheProgrammer@users.noreply.github.com>
2026-03-01 14:32:47 -0700

    feat: allow events to be modified

--- pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1_0/events/player.rs
+++ pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1_0/events/player.rs
@@ -1,15 +1,17 @@
+use wasmtime::component::Resource;
+
 use crate::plugin::{
     loader::wasm::wasm_host::{
-        state::PluginHostState,
+        state::{PlayerResource, PluginHostState, TextComponentResource},
         wit::v0_1_0::{
-            events::ToV0_1_0WasmEvent,
+            events::ToFromV0_1_0WasmEvent,
             pumpkin::plugin::event::{Event, PlayerJoinEventData},
         },
     },
     player::player_join::PlayerJoinEvent,
 };
 
-impl ToV0_1_0WasmEvent for PlayerJoinEvent {
+impl ToFromV0_1_0WasmEvent for PlayerJoinEvent {
     fn to_v0_1_0_wasm_event(&self, state: &mut PluginHostState) -> Event {
         let player_resource = state
             .add_player(self.player.clone())
@@ -19,8 +21,34 @@ impl ToV0_1_0WasmEvent for PlayerJoinEvent {
 
         Event::PlayerJoinEvent(PlayerJoinEventData {
             player: player_resource,
-            text_component: text_component_resource,
+            join_message: text_component_resource,
             cancelled: self.cancelled,
         })
+    }
+
+    fn from_v0_1_0_wasm_event(
+        event: crate::plugin::loader::wasm::wasm_host::wit::v0_1_0::pumpkin::plugin::event::Event,
+        state: &mut PluginHostState,
+    ) -> Self {
+        match event {
+            Event::PlayerJoinEvent(data) => {
+                let player_resource = state
+                    .resource_table
+                    .delete::<PlayerResource>(Resource::new_own(data.player.rep()))
+                    .unwrap();
+
+                let text_component_resource = state
+                    .resource_table
+                    .delete::<TextComponentResource>(Resource::new_own(data.join_message.rep()))
+                    .unwrap();
+
+                PlayerJoinEvent {
+                    player: player_resource.provider,
+                    join_message: text_component_resource.provider,
+                    cancelled: data.cancelled,
+                }
+            }
+            _ => panic!("unexpected event type"),
+        }
     }
 }
