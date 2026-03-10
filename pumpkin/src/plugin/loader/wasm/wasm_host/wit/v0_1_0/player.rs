30247e0682fd80523910f86480ad070e98124a66
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-03-10 10:02:46 +0100

    fix fn order to match trait
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>

--- pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1_0/player.rs
+++ pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1_0/player.rs
@@ -7,13 +7,6 @@ use crate::plugin::loader::wasm::wasm_host::{
 
 impl pumpkin::plugin::player::Host for PluginHostState {}
 impl pumpkin::plugin::player::HostPlayer for PluginHostState {
-    async fn drop(&mut self, rep: Resource<Player>) -> wasmtime::Result<()> {
-        let _ = self
-            .resource_table
-            .delete::<PlayerResource>(Resource::new_own(rep.rep()));
-        Ok(())
-    }
-
     async fn get_id(&mut self, player: Resource<Player>) -> String {
         let resource = self
             .resource_table
@@ -32,5 +25,12 @@ impl pumpkin::plugin::player::HostPlayer for PluginHostState {
             .downcast_ref::<PlayerResource>()
             .expect("resource type mismatch");
         resource.provider.gameprofile.name.clone()
+    }
+
+    async fn drop(&mut self, rep: Resource<Player>) -> wasmtime::Result<()> {
+        let _ = self
+            .resource_table
+            .delete::<PlayerResource>(Resource::new_own(rep.rep()));
+        Ok(())
     }
 }
