5e575192f0c33bdbe82ae8f0f81b194f06772439
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-03-10 10:56:49 +0100

    fix fn order
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>

--- pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1_0/server.rs
+++ pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1_0/server.rs
@@ -39,11 +39,6 @@ impl DowncastResourceExt<ServerResource> for Resource<Server> {
 impl pumpkin::plugin::server::Host for PluginHostState {}
 
 impl pumpkin::plugin::server::HostServer for PluginHostState {
-    async fn drop(&mut self, rep: Resource<Server>) -> wasmtime::Result<()> {
-        rep.consume(self);
-        Ok(())
-    }
-
     async fn get_difficulty(&mut self, server: Resource<Server>) -> Difficulty {
         match server.downcast_ref(self).provider.get_difficulty() {
             pumpkin_util::Difficulty::Peaceful => Difficulty::Peaceful,
@@ -75,5 +70,10 @@ impl pumpkin::plugin::server::HostServer for PluginHostState {
 
     async fn set_motd(&mut self, server: Resource<Server>, motd: String) {
         server.downcast_ref(self).provider.set_motd(motd).await;
+    }
+
+    async fn drop(&mut self, rep: Resource<Server>) -> wasmtime::Result<()> {
+        rep.consume(self);
+        Ok(())
     }
 }
