4be69fcabcc408c388be528183fcd23001faed61
WhiteProject1 <Odunyasi167@gmail.com>
2026-03-17 18:35:43 +0300

    fix: add clippy too_many_lines allow for default_dispatcher

--- pumpkin/src/command/commands/mod.rs
+++ pumpkin/src/command/commands/mod.rs
@@ -64,6 +64,7 @@ mod whitelist;
 mod worldborder;
 
 #[must_use]
+#[expect(clippy::too_many_lines)]
 pub async fn default_dispatcher(
     registry: &RwLock<PermissionRegistry>,
     basic_config: &BasicConfiguration,
