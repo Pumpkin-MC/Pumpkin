95e80b13c050932a4c00a8cabb18c2b010f7bc0d
SomeYellowGuy <30015613+SomeYellowGuy@users.noreply.github.com>
2026-03-29 23:34:08 +0530

    fixed formatting and used clippy

--- pumpkin/src/command/argument_types/time.rs
+++ pumpkin/src/command/argument_types/time.rs
@@ -1,11 +1,13 @@
-use pumpkin_data::translation;
 use crate::command::argument_types::argument_type::{ArgumentType, JavaClientArgumentType};
 use crate::command::errors::command_syntax_error::CommandSyntaxError;
 use crate::command::errors::error_types::CommandErrorType;
 use crate::command::string_reader::StringReader;
+use pumpkin_data::translation;
 
-pub const INVALID_UNIT_ERROR_TYPE: CommandErrorType<0> = CommandErrorType::new(translation::ARGUMENT_TIME_INVALID_UNIT);
-pub const TICK_COUNT_TOO_LOW_ERROR_TYPE: CommandErrorType<0> = CommandErrorType::new(translation::ARGUMENT_TIME_TICK_COUNT_TOO_LOW);
+pub const INVALID_UNIT_ERROR_TYPE: CommandErrorType<0> =
+    CommandErrorType::new(translation::ARGUMENT_TIME_INVALID_UNIT);
+pub const TICK_COUNT_TOO_LOW_ERROR_TYPE: CommandErrorType<0> =
+    CommandErrorType::new(translation::ARGUMENT_TIME_TICK_COUNT_TOO_LOW);
 
 /// Represents an argument type parsing a time value, which can
 /// have one of these suffixes:
@@ -17,18 +19,19 @@ pub const TICK_COUNT_TOO_LOW_ERROR_TYPE: CommandErrorType<0> = CommandErrorType:
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct TimeArgumentType {
     /// The minimum amount of time, in ticks, that this type accepts.
-    pub min: i32
+    pub min: i32,
 }
 
 impl TimeArgumentType {
-
     /// Returns a [`TimeArgumentType`] that accepts any value of time.
-    pub fn any() -> Self {
+    #[must_use]
+    pub const fn any() -> Self {
         Self::new(0)
     }
 
     /// Returns a [`TimeArgumentType`] that accepts any value of time lasting for at least `min` ticks.
-    pub fn new(min: i32) -> Self {
+    #[must_use]
+    pub const fn new(min: i32) -> Self {
         Self { min }
     }
 }
@@ -55,9 +58,7 @@ impl ArgumentType for TimeArgumentType {
     }
 
     fn client_side_parser(&'_ self) -> JavaClientArgumentType<'_> {
-        JavaClientArgumentType::Time {
-            min: self.min
-        }
+        JavaClientArgumentType::Time { min: self.min }
     }
 
     fn examples(&self) -> Vec<String> {
@@ -68,7 +69,7 @@ impl ArgumentType for TimeArgumentType {
 #[cfg(test)]
 mod test {
     use crate::command::{
-        argument_types::{argument_type::ArgumentType, time::TimeArgumentType, time},
+        argument_types::{argument_type::ArgumentType, time, time::TimeArgumentType},
         string_reader::StringReader,
     };
 
@@ -79,14 +80,22 @@ mod test {
 
         assert_parse_ok_reset!(&mut reader, TimeArgumentType::any(), 15);
         assert_parse_ok_reset!(&mut reader, TimeArgumentType::new(10), 15);
-        assert_parse_err_reset!(&mut reader, TimeArgumentType::new(20), &time::TICK_COUNT_TOO_LOW_ERROR_TYPE);
+        assert_parse_err_reset!(
+            &mut reader,
+            TimeArgumentType::new(20),
+            &time::TICK_COUNT_TOO_LOW_ERROR_TYPE
+        );
 
         // Unit
         let mut reader = StringReader::new("95t");
 
         assert_parse_ok_reset!(&mut reader, TimeArgumentType::any(), 95);
         assert_parse_ok_reset!(&mut reader, TimeArgumentType::new(50), 95);
-        assert_parse_err_reset!(&mut reader, TimeArgumentType::new(150), &time::TICK_COUNT_TOO_LOW_ERROR_TYPE);
+        assert_parse_err_reset!(
+            &mut reader,
+            TimeArgumentType::new(150),
+            &time::TICK_COUNT_TOO_LOW_ERROR_TYPE
+        );
     }
 
     #[test]
@@ -103,7 +112,11 @@ mod test {
         assert_parse_ok_reset!(&mut reader, TimeArgumentType::any(), 93);
 
         let mut reader = StringReader::new("-0.2s");
-        assert_parse_err_reset!(&mut reader, TimeArgumentType::any(), &time::TICK_COUNT_TOO_LOW_ERROR_TYPE);
+        assert_parse_err_reset!(
+            &mut reader,
+            TimeArgumentType::any(),
+            &time::TICK_COUNT_TOO_LOW_ERROR_TYPE
+        );
 
         // Minecraft days
         let mut reader = StringReader::new("9d");
@@ -113,8 +126,16 @@ mod test {
 
         // Invalid units
         let mut reader = StringReader::new("14m");
-        assert_parse_err_reset!(&mut reader, TimeArgumentType::any(), &time::INVALID_UNIT_ERROR_TYPE);
+        assert_parse_err_reset!(
+            &mut reader,
+            TimeArgumentType::any(),
+            &time::INVALID_UNIT_ERROR_TYPE
+        );
         let mut reader = StringReader::new("1w");
-        assert_parse_err_reset!(&mut reader, TimeArgumentType::any(), &time::INVALID_UNIT_ERROR_TYPE);
+        assert_parse_err_reset!(
+            &mut reader,
+            TimeArgumentType::any(),
+            &time::INVALID_UNIT_ERROR_TYPE
+        );
     }
 }
