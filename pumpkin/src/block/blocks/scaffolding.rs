054c834916f905fcdd960bb608eafc3190ffebf2
Musawer <118316797+TheRealMusawer@users.noreply.github.com>
2026-03-31 16:44:57 -0400

    Hopefully last error
    Signed-off-by: Musawer <118316797+TheRealMusawer@users.noreply.github.com>

--- pumpkin/src/block/blocks/scaffolding.rs
+++ pumpkin/src/block/blocks/scaffolding.rs
@@ -80,7 +80,7 @@ impl BlockBehaviour for ScaffoldingBlock {
 
     fn on_scheduled_tick<'a>(&'a self, args: OnScheduledTickArgs<'a>) -> BlockFuture<'a, ()> {
         Box::pin(async move {
-            let world: &dyn BlockAccessor = &*args.world;
+            let world: &dyn BlockAccessor = &*args.world as &World;
 
             if !can_survive(world, args.position).await {
                 args.world
