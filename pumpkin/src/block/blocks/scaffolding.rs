7f1f887fffe3b8c4f636413d989032af922ba075
Musawer <118316797+TheRealMusawer@users.noreply.github.com>
2026-04-01 07:49:51 -0400

    Refactor world access in ScaffoldingBlock methods
    Signed-off-by: Musawer <118316797+TheRealMusawer@users.noreply.github.com>

--- pumpkin/src/block/blocks/scaffolding.rs
+++ pumpkin/src/block/blocks/scaffolding.rs
@@ -19,7 +19,8 @@ pub struct ScaffoldingBlock;
 
 impl BlockBehaviour for ScaffoldingBlock {
     fn on_place<'a>(&'a self, args: OnPlaceArgs<'a>) -> BlockFuture<'a, BlockStateId> {
-        let world: &dyn BlockAccessor = &**args.world;
+        // Use block_accessor if OnPlaceArgs provides it, otherwise use the stable deref
+        let world = args.block_accessor; 
         Box::pin(async move {
             let mut props = ScaffoldingLikeProperties::default(args.block);
             props.waterlogged = args.replacing.water_source();
@@ -66,7 +67,7 @@ impl BlockBehaviour for ScaffoldingBlock {
         &'a self,
         args: GetStateForNeighborUpdateArgs<'a>,
     ) -> BlockFuture<'a, BlockStateId> {
-        let world: &dyn BlockAccessor = &**args.world;
+        let world = args.block_accessor;
         Box::pin(async move {
             let mut props = ScaffoldingLikeProperties::from_state_id(args.state_id, args.block);
 
@@ -81,7 +82,7 @@ impl BlockBehaviour for ScaffoldingBlock {
     }
 
     fn on_scheduled_tick<'a>(&'a self, args: OnScheduledTickArgs<'a>) -> BlockFuture<'a, ()> {
-        let world: &dyn BlockAccessor = &**args.world;
+        let world = args.block_accessor;
         Box::pin(async move {
             if !can_survive(world, args.position).await {
                 args.world
