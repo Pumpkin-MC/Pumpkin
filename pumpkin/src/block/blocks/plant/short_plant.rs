3b8c7f836dc2ee94763b4fbb57b07f92afa958c8
Lucachinou <lucachinou@duck.com>
2026-04-07 12:05:38 +0200

    Implemented seeds drop

--- pumpkin/src/block/blocks/plant/short_plant.rs
+++ pumpkin/src/block/blocks/plant/short_plant.rs
@@ -1,10 +1,10 @@
-use pumpkin_data::Block;
+use rand::RngExt;
+use pumpkin_data::{Block, Enchantment};
+use pumpkin_data::item::Item;
+use pumpkin_data::item_stack::ItemStack;
 use pumpkin_world::BlockStateId;
 
-use crate::block::{
-    BlockBehaviour, BlockFuture, BlockMetadata, CanPlaceAtArgs, GetStateForNeighborUpdateArgs,
-    blocks::plant::PlantBlockBase,
-};
+use crate::block::{BlockBehaviour, BlockFuture, BlockMetadata, CanPlaceAtArgs, GetStateForNeighborUpdateArgs, blocks::plant::PlantBlockBase, BrokenArgs};
 
 pub struct ShortPlantBlock;
 
@@ -21,6 +21,24 @@ impl BlockBehaviour for ShortPlantBlock {
         })
     }
 
+    fn broken<'a>(&'a self, _args: BrokenArgs<'a>) -> BlockFuture<'a, ()> {
+        Box::pin(async move {
+            let held = _args.player.inventory.held_item();
+            let held = held.lock().await;
+            if held.item == &Item::SHEARS || held.get_enchantment_level(&Enchantment::SILK_TOUCH) > 0 {
+                return;
+            }
+
+            drop(held);
+            if rand::rng().random_bool(0.125) {
+                _args.world
+                    .drop_stack(_args.position, ItemStack::new(1, &Item::WHEAT_SEEDS))
+                    .await;
+            }
+
+        })
+    }
+
     fn get_state_for_neighbor_update<'a>(
         &'a self,
         args: GetStateForNeighborUpdateArgs<'a>,
@@ -32,9 +50,10 @@ impl BlockBehaviour for ShortPlantBlock {
                 args.position,
                 args.state_id,
             )
-            .await
+                .await
         })
     }
 }
 
 impl PlantBlockBase for ShortPlantBlock {}
+
