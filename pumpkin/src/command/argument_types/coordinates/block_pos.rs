477cc4462734913db3262c4e3f8992e2f53e427d
SomeYellowGuy <30015613+SomeYellowGuy@users.noreply.github.com>
2026-04-09 15:57:33 +0530

    switches to using argument type helpers and added `spawnable_pos` method

--- pumpkin/src/command/argument_types/coordinates/block_pos.rs
+++ pumpkin/src/command/argument_types/coordinates/block_pos.rs
@@ -1,17 +1,24 @@
 use crate::command::argument_types::argument_type::{ArgumentType, JavaClientArgumentType};
-use crate::command::argument_types::coordinates::Coordinates;
+use crate::command::argument_types::coordinates::{
+    Coordinates, NOT_LOADED_ERROR_TYPE, OUT_OF_BOUNDS_ERROR_TYPE,
+};
+use crate::command::context::command_context::CommandContext;
 use crate::command::errors::command_syntax_error::CommandSyntaxError;
 use crate::command::string_reader::StringReader;
+use crate::world::World;
+use pumpkin_util::math::position::BlockPos;
 
 /// An argument type for a 3-dimensional vector representing a block position.
 ///
-/// The returned [`Coordinates`] can be converted to a [`BlockPos`] via one of the
-/// following methods:
-/// - [`Coordinates::to_block_pos`]: Normal conversion that always succeeds.
-/// - [`Coordinates::try_to_loaded_block_pos`]: Converts the coordinates to a *loaded* `BlockPos`.
-///   This may not succeed. This is what you want to use most of the time.
-/// - [`Coordinates::try_to_loaded_block_pos_in_world`]: Converts the coordinates to a *loaded* `BlockPos`.
-///   in the provided world. This may not succeed.
+/// The parsed [`Coordinates`] can be converted to a [`BlockPos`] via one of the
+/// following associated methods:
+/// - [`BlockPosArgumentType::get_block_pos`]: Normal conversion.
+/// - [`BlockPosArgumentType::get_loaded_block_pos`]: Converts the coordinates to a *loaded* `BlockPos`.
+///   This is what you want to use most of the time (if you need to update the loaded position somehow).
+/// - [`BlockPosArgumentType::get_loaded_block_pos_in_world`]: Converts the coordinates to a *loaded* `BlockPos`
+///   in the provided world.
+/// - [`BlockPosArgumentType::get_spawnable_pos`]: Converts the coordinates to a `BlockPos` where
+///   a player can spawn.
 pub struct BlockPosArgumentType;
 
 impl ArgumentType for BlockPosArgumentType {
@@ -31,5 +38,92 @@ impl ArgumentType for BlockPosArgumentType {
 
     fn examples(&self) -> Vec<String> {
         examples!("1 3 5", "-3 ~24 ~-1", "80 80 80", "^ ^9 ^56")
+    }
+}
+
+impl BlockPosArgumentType {
+    /// Returns a [`CommandContext`]'s parsed coordinate argument in the form of a [`BlockPos`].
+    ///
+    /// # Arguments
+    /// * `context` - The [`CommandContext`] that has the parsed `Coordinates` with the provided argument name.
+    /// * `name` - The name of the argument that was parsed.
+    ///
+    /// # Returns
+    /// The loaded `BlockPos` containing the position represented by the parsed argument, wrapped in an `Ok`,
+    /// or an `Err` with the appropriate [`CommandSyntaxError`] if it could not be resolved.
+    pub fn get_block_pos(
+        context: &CommandContext,
+        name: &str,
+    ) -> Result<BlockPos, CommandSyntaxError> {
+        Ok(BlockPos::floored_v(
+            context
+                .get_argument::<Coordinates>(name)?
+                .resolve(context.source.as_ref()),
+        ))
+    }
+
+    /// Returns a [`CommandContext`]'s parsed coordinate argument in the form of a loaded [`BlockPos`].
+    ///
+    /// # Arguments
+    /// * `context` - The [`CommandContext`] that has the parsed `Coordinates` with the provided argument name.
+    ///   Its `CommandSource` also decides the world to check the load status in.
+    /// * `name` - The name of the argument that was parsed.
+    ///
+    /// # Returns
+    /// The loaded `BlockPos` containing the position represented by the parsed argument, wrapped in an `Ok`,
+    /// or an `Err` with the appropriate [`CommandSyntaxError`] if it could not be resolved.
+    pub fn get_loaded_block_pos(
+        context: &CommandContext,
+        name: &str,
+    ) -> Result<BlockPos, CommandSyntaxError> {
+        Self::get_loaded_block_pos_in_world(context, name, context.source.world())
+    }
+
+    /// Returns a [`CommandContext`]'s parsed coordinate argument in the form of a loaded [`BlockPos`]
+    /// in the provided world.
+    ///
+    /// # Arguments
+    /// * `context` - The [`CommandContext`] that has the parsed `Coordinates` with the provided argument name.
+    /// * `name` - The name of the argument that was parsed.
+    /// * `world` - The world to check the load status in.
+    ///
+    /// # Returns
+    /// The loaded `BlockPos` containing the position represented by the parsed argument, wrapped in an `Ok`,
+    /// or an `Err` with the appropriate [`CommandSyntaxError`] if it could not be resolved.
+    pub fn get_loaded_block_pos_in_world(
+        context: &CommandContext,
+        name: &str,
+        world: &World,
+    ) -> Result<BlockPos, CommandSyntaxError> {
+        let pos = Self::get_block_pos(context, name)?;
+        if world.level.try_get_chunk(&pos.chunk_position()).is_none() {
+            Err(NOT_LOADED_ERROR_TYPE.create_without_context())
+        } else if !world.is_in_build_limit(pos) {
+            Err(OUT_OF_BOUNDS_ERROR_TYPE.create_without_context())
+        } else {
+            Ok(pos)
+        }
+    }
+
+    /// Returns a [`CommandContext`]'s parsed coordinate argument in the form of a [`BlockPos`]
+    /// where players can spawn.
+    ///
+    /// # Arguments
+    /// * `context` - The [`CommandContext`] that has the parsed `Coordinates` with the provided argument name.
+    /// * `name` - The name of the argument that was parsed.
+    ///
+    /// # Returns
+    /// The loaded `BlockPos` containing the position represented by the parsed argument, wrapped in an `Ok`,
+    /// or an `Err` with the appropriate [`CommandSyntaxError`] if it could not be resolved.
+    pub fn get_spawnable_pos(
+        context: &CommandContext,
+        name: &str,
+    ) -> Result<BlockPos, CommandSyntaxError> {
+        let pos = Self::get_block_pos(context, name)?;
+        if World::is_valid(pos) {
+            Ok(pos)
+        } else {
+            Err(OUT_OF_BOUNDS_ERROR_TYPE.create_without_context())
+        }
     }
 }
