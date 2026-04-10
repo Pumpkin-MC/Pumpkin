6a2fb0a83988286ec443b99e6cd1ead1ca6d49e2
SomeYellowGuy <30015613+SomeYellowGuy@users.noreply.github.com>
2026-04-10 22:07:21 +0530

    fixed formatting and clippy

--- pumpkin/src/command/argument_types/game_profile.rs
+++ pumpkin/src/command/argument_types/game_profile.rs
@@ -1,5 +1,3 @@
-use uuid::Uuid;
-use pumpkin_data::translation;
 use crate::command::argument_types::argument_type::{ArgumentType, JavaClientArgumentType};
 use crate::command::argument_types::entity::ONLY_PLAYERS_ALLOWED_ERROR_TYPE;
 use crate::command::argument_types::entity_selector::EntitySelector;
@@ -10,42 +8,42 @@ use crate::command::errors::command_syntax_error::CommandSyntaxError;
 use crate::command::errors::error_types::CommandErrorType;
 use crate::command::string_reader::StringReader;
 use crate::net::GameProfile;
+use pumpkin_data::translation;
+use uuid::Uuid;
 
-pub const UNKNOWN_PLAYER_ERROR_TYPE: CommandErrorType<0> = CommandErrorType::new(translation::ARGUMENT_PLAYER_UNKNOWN);
+pub const UNKNOWN_PLAYER_ERROR_TYPE: CommandErrorType<0> =
+    CommandErrorType::new(translation::ARGUMENT_PLAYER_UNKNOWN);
 
 /// A result from the [`GameProfileArgumentType`], which can be resolved into
 /// one or more [`GameProfile`]s, successfully or not.
 pub enum GameProfileResult {
-    Selector(EntitySelector),
+    Selector(Box<EntitySelector>),
     Name(String),
     Uuid(Uuid),
 }
 
 impl GameProfileResult {
     /// Resolves this result with the help of a [`CommandSource`].
-    pub async fn resolve(&self, source: &CommandSource) -> Result<Vec<GameProfile>, CommandSyntaxError> {
+    pub async fn resolve(
+        &self,
+        source: &CommandSource,
+    ) -> Result<Vec<GameProfile>, CommandSyntaxError> {
         let players = match self {
             Self::Selector(selector) => selector.find_players(source).await,
-            Self::Name(name) => source.server()
-                    .get_player_by_name(name.as_str())
-                    .map_or_else(
-                        || Err(UNKNOWN_PLAYER_ERROR_TYPE.create_without_context()),
-                        |p| Ok(vec![p])
-                    ),
-            Self::Uuid(uuid) => source.server()
-                .get_player_by_uuid(*uuid)
+            Self::Name(name) => source
+                .server()
+                .get_player_by_name(name.as_str())
                 .map_or_else(
                     || Err(UNKNOWN_PLAYER_ERROR_TYPE.create_without_context()),
-                    |p| Ok(vec![p])
-                )
+                    |p| Ok(vec![p]),
+                ),
+            Self::Uuid(uuid) => source.server().get_player_by_uuid(*uuid).map_or_else(
+                || Err(UNKNOWN_PLAYER_ERROR_TYPE.create_without_context()),
+                |p| Ok(vec![p]),
+            ),
         }?;
 
-        Ok(
-            players.iter()
-            .map(|p| &p.gameprofile)
-            .cloned()
-            .collect()
-        )
+        Ok(players.iter().map(|p| &p.gameprofile).cloned().collect())
     }
 }
 
@@ -61,7 +59,7 @@ impl ArgumentType for GameProfileArgumentType {
     type Item = GameProfileResult;
 
     fn parse(&self, reader: &mut StringReader) -> Result<Self::Item, CommandSyntaxError> {
-        self.parse_with_allow_selectors(reader, true)
+        Self::parse_with_allow_selectors(reader, true)
     }
 
     fn client_side_parser(&'_ self) -> JavaClientArgumentType<'_> {
@@ -69,18 +67,12 @@ impl ArgumentType for GameProfileArgumentType {
     }
 
     fn examples(&self) -> Vec<String> {
-        examples!(
-            "Herobrine",
-            "98765",
-            "@a",
-            "@p[limit=2]"
-        )
+        examples!("Herobrine", "98765", "@a", "@p[limit=2]")
     }
 }
 
 impl GameProfileArgumentType {
     fn parse_with_allow_selectors(
-        self,
         reader: &mut StringReader,
         allow_selectors: bool,
     ) -> Result<<Self as ArgumentType>::Item, CommandSyntaxError> {
@@ -91,7 +83,7 @@ impl GameProfileArgumentType {
             if selector.includes_entities {
                 Err(ONLY_PLAYERS_ALLOWED_ERROR_TYPE.create(reader))
             } else {
-                Ok(GameProfileResult::Selector(selector))
+                Ok(GameProfileResult::Selector(Box::new(selector)))
             }
         } else {
             // We read a UUID or player name.
@@ -100,17 +92,21 @@ impl GameProfileArgumentType {
                 reader.skip();
             }
             let string = &reader.string()[i..reader.cursor()];
-            if let Ok(uuid) = Uuid::try_parse(string) {
-                Ok(GameProfileResult::Uuid(uuid))
-            } else {
-                Ok(GameProfileResult::Name(string.to_owned()))
-            }
+            Ok(Uuid::try_parse(string).map_or_else(
+                |_| GameProfileResult::Name(string.to_owned()),
+                GameProfileResult::Uuid,
+            ))
         }
     }
 
     /// Tries to get any number of [`GameProfile`]s from a parsed argument of the provided [`CommandContext`].
-    pub async fn get(context: &CommandContext<'_>, name: &str) -> Result<Vec<GameProfile>, CommandSyntaxError> {
-        context.get_argument::<GameProfileResult>(name)?
-            .resolve(context.source.as_ref()).await
+    pub async fn get(
+        context: &CommandContext<'_>,
+        name: &str,
+    ) -> Result<Vec<GameProfile>, CommandSyntaxError> {
+        context
+            .get_argument::<GameProfileResult>(name)?
+            .resolve(context.source.as_ref())
+            .await
     }
 }
