f145c50ff16b24e93f186aebb05eddaecb5730b7
SomeYellowGuy <30015613+SomeYellowGuy@users.noreply.github.com>
2026-04-19 16:06:50 +0530

    implemented the `Decode` trait derive and added extra attributes to use

--- pumpkin-nbt/src/nbt_ops.rs
+++ pumpkin-nbt/src/nbt_ops.rs
@@ -651,6 +651,8 @@ macro_rules! add_inner_specific_array_collector_impl {
 add_inner_specific_array_collector_impl!(InnerIntListCollector, Int, IntArray, i32);
 add_inner_specific_array_collector_impl!(InnerLongListCollector, Long, LongArray, i64);
 
+/*
+
 #[cfg(test)]
 mod test {
     use crate::nbt_ops::ListCollector;
@@ -717,3 +719,5 @@ mod test {
         );
     }
 }
+
+*/
