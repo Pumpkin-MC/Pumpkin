83f2c26b55e0855c174b5799383631722fd9187b
SomeYellowGuy <30015613+SomeYellowGuy@users.noreply.github.com>
2026-04-19 22:04:55 +0530

    added support for enums whose variants are all units and removed `NbtOps` tests for now

--- pumpkin-nbt/src/nbt_ops.rs
+++ pumpkin-nbt/src/nbt_ops.rs
@@ -662,8 +662,6 @@ macro_rules! add_inner_specific_array_collector_impl {
 add_inner_specific_array_collector_impl!(InnerIntListCollector, Int, IntArray, i32);
 add_inner_specific_array_collector_impl!(InnerLongListCollector, Long, LongArray, i64);
 
-/*
-
 #[cfg(test)]
 mod test {
     use crate::nbt_ops::ListCollector;
@@ -730,5 +728,3 @@ mod test {
         );
     }
 }
-
-*/
