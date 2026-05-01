068ba1cce5d19671d20c0936899bdb1fbff3e88c
Jakub Palacky <kubik.palacky@gmail.com>
2026-05-01 12:38:07 +0200

    Fix clippy

--- pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1/player.rs
+++ pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1/player.rs
@@ -319,13 +319,14 @@ impl pumpkin::plugin::player::HostPlayer for PluginHostState {
     ) -> wasmtime::Result<Option<Resource<pumpkin::plugin::text::TextComponent>>> {
         let player = player_from_resource(self, &player)?;
         let tab_list_name = player.get_tab_list_name().await;
-        match tab_list_name {
-            Some(name) => self
-                .add_text_component(name)
-                .map(Some)
-                .map_err(|_| wasmtime::Error::msg("failed to add text-component resource")),
-            None => Ok(None),
-        }
+        tab_list_name.map_or_else(
+            || Ok(None),
+            |name| {
+                self.add_text_component(name)
+                    .map(Some)
+                    .map_err(|_| wasmtime::Error::msg("failed to add text-component resource"))
+            },
+        )
     }
 
     async fn set_tab_list_name(
