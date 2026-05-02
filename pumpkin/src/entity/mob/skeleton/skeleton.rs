755a1bd74934ecf06478ec7191e664d85d8f4a62
Alexander Medvedev <lilalexmed@proton.me>
2026-05-02 22:04:26 +0200

    chore: more entities taking sun damage
    instead of hardcoding, it now uses the tag::EntityType::MINECRAFT_BURN_IN_DAYLIGHT, will definitely be more future proof as well

--- pumpkin/src/entity/mob/skeleton/skeleton.rs
+++ pumpkin/src/entity/mob/skeleton/skeleton.rs
@@ -1,8 +1,7 @@
 use std::sync::Arc;
 
-use crate::entity::mob::SunSensitive;
 use crate::entity::{
-    Entity, EntityBase, EntityBaseFuture, NBTStorage,
+    Entity, NBTStorage,
     mob::{Mob, MobEntity, skeleton::SkeletonEntityBase},
 };
 
@@ -24,10 +23,4 @@ impl Mob for SkeletonEntity {
     fn get_mob_entity(&self) -> &MobEntity {
         &self.entity.mob_entity
     }
-
-    fn mob_tick<'a>(&'a self, _caller: &'a Arc<dyn EntityBase>) -> EntityBaseFuture<'a, ()> {
-        self.sun_sensitive_tick()
-    }
 }
-
-impl SunSensitive for SkeletonEntity {}
