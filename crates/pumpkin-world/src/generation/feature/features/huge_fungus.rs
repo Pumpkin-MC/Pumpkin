7324bcb3fc31b8cf994165c1c8c6e459016c3abd
zhangxuan2011 <zx20110412@outlook.com>
2026-07-25 08:48:26 +0800

    fix(world/generate/huge_fungus): :bug: Fixed something wrong about the huge fungus generation
    - Added some checks to decide should we generate this fungus or not
     - Added biome check instead of random to decide fungus type through biome
     - Added block check to avoid fungus generates on incorrect block
    
    #2466

--- crates/pumpkin-world/src/generation/feature/features/huge_fungus.rs
+++ crates/pumpkin-world/src/generation/feature/features/huge_fungus.rs
@@ -1,4 +1,4 @@
-use pumpkin_data::Block;
+use pumpkin_data::{Block, chunk::Biome, tag};
 use pumpkin_util::{math::position::BlockPos, random::RandomGenerator, random::RandomImpl};
 
 use crate::generation::proto_chunk::GenerationCache;
@@ -16,8 +16,39 @@ impl HugeFungusFeature {
         random: &mut RandomGenerator,
         pos: BlockPos,
     ) -> bool {
+        /* Here we'd like to check something about the
+         * position, like if it's valid for a huge fungus to grow there.
+         */
+        let root_pos = pos;
+
+        // Check: Is this generated in air? (Expected: yes)
+        if !chunk.is_air(&root_pos.0) {
+            return false;
+        }
+
+        // Check: Is the block below a valid ground (nylium or netherrack) (Expected: yes)?
+        let below_state = GenerationCache::get_block_state(chunk, &root_pos.down().0);
+        let is_valid_block = below_state
+            .to_block_id()
+            .has_tag(tag::Block::MINECRAFT_NYLIUM)
+            || below_state.to_block_id().has_tag(tag::Block::C_NETHERRACKS);
+        if !is_valid_block {
+            return false;
+        }
+
+        /* Main generation logic */
         let height = random.next_bounded_i32(4) + 6;
-        let is_warped = random.next_bool();
+        let is_warped = {
+            // Get current biome
+            let biome = chunk.get_biome_for_terrain_gen(pos.0.x, pos.0.y, pos.0.z);
+
+            // And match...
+            match biome {
+                b if b == &Biome::CRIMSON_FOREST => false,
+                b if b == &Biome::WARPED_FOREST => true,
+                _ => return false, // Should not be generated if not in those biome
+            }
+        };
 
         let stem_state = if is_warped {
             Block::WARPED_STEM.default_state
