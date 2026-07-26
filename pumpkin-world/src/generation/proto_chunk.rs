2b2985522dfd8d1fc5e9bba0571df1d031a5384b
Akorn27 <sekmaks4@gmail.com>
2026-07-26 12:27:15 +0200

    Fixup formatting

--- pumpkin-world/src/generation/proto_chunk.rs
+++ pumpkin-world/src/generation/proto_chunk.rs
@@ -503,11 +503,13 @@ impl ProtoChunk {
     #[inline]
     #[must_use]
     pub fn get_biome_id(&self, x: i32, y: i32, z: i32) -> u8 {
-        let index = self.local_biome_pos_to_biome_index(
-            x & biome_coords::from_block(15),
-            y - biome_coords::from_block(self.bottom_y() as i32),
-            z & biome_coords::from_block(15),
-        ).clamp(0, self.flat_biome_map.len() - 1);
+        let index = self
+            .local_biome_pos_to_biome_index(
+                x & biome_coords::from_block(15),
+                y - biome_coords::from_block(self.bottom_y() as i32),
+                z & biome_coords::from_block(15),
+            )
+            .clamp(0, self.flat_biome_map.len() - 1);
         self.flat_biome_map[index]
     }
 
