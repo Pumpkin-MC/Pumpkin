3f7bbcf78938885b970f07471720da5326c95780
andbix45 <andbix45@gmail.com>
2026-07-28 20:58:59 +0200

    feat(server): support F3+2 tick debug sample chart

--- pumpkin/src/net/java/mod.rs
+++ pumpkin/src/net/java/mod.rs
@@ -16,7 +16,8 @@ use pumpkin_protocol::java::server::play::{
     SAttack, SBundleItemSelected, SChangeGameMode, SChatCommand, SChatMessage, SChunkBatch,
     SClickSlot, SClientCommand, SClientInformationPlay, SClientTickEnd, SCloseContainer,
     SCommandSuggestion, SConfirmTeleport, SContainerButtonClick,
-    SCookieResponse as SPCookieResponse, SCustomPayload, SInteract, SJigsawGenerate, SMoveVehicle,
+    SCookieResponse as SPCookieResponse, SCustomPayload, SDebugSampleSubscription,
+    SDebugSubscriptionRequest, SInteract, SJigsawGenerate, SMoveVehicle,
     SPaddleBoat, SPickItemFromBlock, SPlaceRecipe, SPlayPingRequest, SPlayerAbilities,
     SPlayerAction, SPlayerCommand, SPlayerInput, SPlayerLoaded, SPlayerPosition,
     SPlayerPositionRotation, SPlayerRotation, SPlayerSession, SRecipeBookChangeSettings,
@@ -1010,6 +1011,18 @@ impl JavaClient {
             }
             id if id == SSetTestBlock::to_id(version) => {
                 self.handle_set_test_block(player, &SSetTestBlock::read(&mut payload, &version)?);
+            }
+            id if id == SDebugSubscriptionRequest::to_id(version) => {
+                self.handle_debug_subscription_request(
+                    player,
+                    &SDebugSubscriptionRequest::read(&mut payload, &version)?,
+                );
+            }
+            id if id == SDebugSampleSubscription::to_id(version) => {
+                self.handle_debug_sample_subscription(
+                    player,
+                    &SDebugSampleSubscription::read(&mut payload, &version)?,
+                );
             }
             id if id == SPlayerPosition::to_id(version) => {
                 self.handle_position(
