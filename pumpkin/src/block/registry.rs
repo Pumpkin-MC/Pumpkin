3e1ecb565842f27b74981e070625306d09b9a82b
yhypno <yashwantbora119@gmail.com>
2026-07-29 13:50:16 +0530

    fix(block): damage shears when carving pumpkins

--- pumpkin/src/block/registry.rs
+++ pumpkin/src/block/registry.rs
@@ -145,6 +145,7 @@ use crate::server::Server;
 use crate::world::World;
 use pumpkin_data::BlockStateId;
 use pumpkin_data::block_rotation::{Mirror, Rotation};
+use pumpkin_data::data_component_impl::EquipmentSlot;
 use pumpkin_data::fluid::Fluid;
 use pumpkin_data::item::Item;
 use pumpkin_data::item_stack::ItemStack;
@@ -782,6 +783,7 @@ impl BlockRegistry {
         position: &BlockPos,
         hit: &BlockHitResult<'_>,
         item_stack: &Arc<Mutex<ItemStack>>,
+        equipment_slot: &EquipmentSlot,
         server: &Server,
         world: &Arc<World>,
     ) -> BlockActionResult {
@@ -796,6 +798,7 @@ impl BlockRegistry {
                     player,
                     hit,
                     item_stack,
+                    equipment_slot,
                 })
                 .await;
         }
