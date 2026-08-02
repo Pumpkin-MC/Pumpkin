0dc7dc09ff25729a0c9ff60c95a8ed411e9e1deb
Eshan I. <79593316+eshankiyer@users.noreply.github.com>
2026-08-02 00:30:54 -0400

    fix(item): damage shears when carving pumpkins

--- pumpkin/src/block/blocks/pumpkin.rs
+++ pumpkin/src/block/blocks/pumpkin.rs
@@ -47,7 +47,7 @@ impl crate::block::BlockBehaviour for PumpkinBlock {
                 ItemStack::new(4, &Item::PUMPKIN_SEEDS),
             ));
             args.world.spawn_entity(item_entity).await;
-            // TODO: Deduct 1 durability from held shears (skip in Creative mode).
+            args.player.damage_held_item(1).await;
             BlockActionResult::Consume
         })
     }
