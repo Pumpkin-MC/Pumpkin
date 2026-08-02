c1d387c2cf75d2bbd828bd005fc45c3c217db215
Eshan I. <79593316+eshankiyer@users.noreply.github.com>
2026-08-01 22:56:44 -0400

    fix(ai): make spiders abandon daylight attacks

--- pumpkin/src/entity/mob/spider.rs
+++ pumpkin/src/entity/mob/spider.rs
@@ -1,16 +1,66 @@
 use std::sync::{Arc, Weak};
 
 use pumpkin_data::entity::EntityType;
+use rand::RngExt;
 
 use crate::entity::{
     Entity, NBTStorage,
     ai::goal::{
-        active_target::ActiveTargetGoal, look_around::RandomLookAroundGoal,
-        look_at_entity::LookAtEntityGoal, melee_attack::MeleeAttackGoal, revenge::RevengeGoal,
-        swim::SwimGoal, wander_around::WanderAroundGoal,
+        Controls, Goal, GoalFuture, active_target::ActiveTargetGoal,
+        look_around::RandomLookAroundGoal, look_at_entity::LookAtEntityGoal,
+        melee_attack::MeleeAttackGoal, revenge::RevengeGoal, swim::SwimGoal,
+        wander_around::WanderAroundGoal,
     },
     mob::{Mob, MobEntity},
 };
+
+struct SpiderAttackGoal {
+    melee: MeleeAttackGoal,
+}
+
+impl SpiderAttackGoal {
+    fn new() -> Self {
+        Self {
+            melee: MeleeAttackGoal::new(1.0, true),
+        }
+    }
+}
+
+impl Goal for SpiderAttackGoal {
+    fn can_start<'a>(&'a mut self, mob: &'a dyn Mob) -> GoalFuture<'a, bool> {
+        self.melee.can_start(mob)
+    }
+
+    fn should_continue<'a>(&'a self, mob: &'a dyn Mob) -> GoalFuture<'a, bool> {
+        Box::pin(async move {
+            let entity = mob.get_entity();
+            let world = entity.world.load();
+            let brightness =
+                world.get_sky_light_level(&entity.get_eye_pos().to_block_pos()) as f32 / 15.0;
+            if brightness >= 0.5 && mob.get_random().random_range(0..100) == 0 {
+                mob.set_mob_target(None).await;
+                return false;
+            }
+            self.melee.should_continue(mob).await
+        })
+    }
+
+    fn start<'a>(&'a mut self, mob: &'a dyn Mob) -> GoalFuture<'a, ()> {
+        self.melee.start(mob)
+    }
+
+    fn stop<'a>(&'a mut self, mob: &'a dyn Mob) -> GoalFuture<'a, ()> {
+        self.melee.stop(mob)
+    }
+
+    fn tick<'a>(&'a mut self, mob: &'a dyn Mob) -> GoalFuture<'a, ()> {
+        self.melee.tick(mob)
+    }
+
+    fn controls(&self) -> Controls {
+        self.melee.controls()
+    }
+}
 
 pub struct SpiderEntity {
     pub mob_entity: MobEntity,
@@ -31,8 +81,7 @@ impl SpiderEntity {
             let mut target_selector = mob_arc.mob_entity.target_selector.lock().unwrap();
 
             goal_selector.add_goal(1, Box::new(SwimGoal::default()));
-            // TODO: SpiderAttackGoal for jumping
-            goal_selector.add_goal(3, Box::new(MeleeAttackGoal::new(1.0, false)));
+            goal_selector.add_goal(3, Box::new(SpiderAttackGoal::new()));
             goal_selector.add_goal(5, Box::new(WanderAroundGoal::new(0.8)));
             goal_selector.add_goal(
                 6,
