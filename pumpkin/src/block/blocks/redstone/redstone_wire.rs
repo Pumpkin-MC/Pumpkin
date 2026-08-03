9733f760fb07b333dd924141e3c5c87fda0411cf
aprettygoodprogramer <evan.j.ducas@gmail.com>
2026-08-02 20:51:47 -0400

    Condense If Statments

--- pumpkin/src/block/blocks/redstone/redstone_wire.rs
+++ pumpkin/src/block/blocks/redstone/redstone_wire.rs
@@ -199,21 +199,20 @@ impl BlockBehaviour for RedstoneWireBlock {
     ) -> BlockFuture<'a, u8> {
         Box::pin(async move {
             let wire = RedstoneWireProperties::from_state_id(args.state.id, args.block);
-
+            
             if args.direction == BlockDirection::Up {
                 return wire.power;
             }
 
-            if let Some(facing) = args.direction.opposite().to_horizontal_facing() {
-                if wire.is_side_connected(facing) {
-                    return wire.power;
-                }
-            }
+             if let Some(facing) = args.direction.opposite().to_horizontal_facing()
+                 && wire.is_side_connected(facing) {
+                     return wire.power;
+                 }
 
             0
         })
     }
-
+    
     fn get_strong_redstone_power<'a>(
         &'a self,
         args: GetRedstonePowerArgs<'a>,
