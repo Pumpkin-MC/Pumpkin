ef3209bd13351e070044066d7572086e16acba77
zhangxuan2011 <zx20110412@outlook.com>
2026-08-08 10:23:30 +0800

    style: :art: Code formatting

--- crates/pumpkin/src/entity/mob/slime.rs
+++ crates/pumpkin/src/entity/mob/slime.rs
@@ -361,7 +361,11 @@ impl Mob for SlimeEntity {
                         pos.y + 0.5,
                         pos.z + zd as f64,
                     );
-                    let new_entity = Entity::new(world.clone(), new_pos, self.entity.living_entity.entity.entity_type);
+                    let new_entity = Entity::new(
+                        world.clone(),
+                        new_pos,
+                        self.entity.living_entity.entity.entity_type,
+                    );
                     let slime_like = Self::new(new_entity);
                     slime_like.set_size(half_size, true);
                     slime_like
