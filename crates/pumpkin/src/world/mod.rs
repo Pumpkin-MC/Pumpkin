0375fbe72e153980d5a7b267f437ccb9489b9793
ZlordHUN <137647604+ZlordHUN@users.noreply.github.com>
2026-08-12 23:09:27 +0200

    Fix Bedrock dead player reconnects

--- crates/pumpkin/src/world/mod.rs
+++ crates/pumpkin/src/world/mod.rs
@@ -1954,7 +1954,6 @@ impl World {
         let level_info = server.level_info.load();
         let weather = self.weather.lock().await;
         let runtime_id = player.entity_id() as u64;
-
         let (position, yaw, pitch) = if player.has_played_before.load(Ordering::Relaxed) {
             let position = player.position();
             let yaw = player.get_entity().yaw.load(); //info.spawn_angle;
@@ -2443,7 +2442,7 @@ impl World {
                     Attribute {
                         min_value: 0.0,
                         max_value: 20.0,
-                        current_value: 20.0,
+                        current_value: player.living_entity.health.load(),
                         default_min_value: 0.0,
                         default_max_value: 20.0,
                         default_value: 20.0,
@@ -2453,7 +2452,7 @@ impl World {
                     Attribute {
                         min_value: 0.0,
                         max_value: 20.0,
-                        current_value: 20.0,
+                        current_value: player.hunger_manager.level.load().into(),
                         default_min_value: 0.0,
                         default_max_value: 20.0,
                         default_value: 20.0,
