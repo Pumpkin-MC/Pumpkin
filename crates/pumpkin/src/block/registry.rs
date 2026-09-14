e7f2d0753d13b9463fe6023e378ed01c46c236c9
BouncingElf10 <168445249+BouncingElf10@users.noreply.github.com>
2026-09-14 18:07:05 +0200

    feat(block): added shulker box item component to store and apply items

--- crates/pumpkin/src/block/registry.rs
+++ crates/pumpkin/src/block/registry.rs
@@ -795,6 +795,12 @@ impl BlockRegistry {
             i32::from(BlockState::to_be_network_id(new_state)),
         );
 
+        if let Ok(hand) = pumpkin_util::Hand::from_packet_id(use_item_on.hand.0)
+            && let Some(block_entity) = world.get_block_entity(&final_block_pos)
+        {
+            block_entity.apply_item_components(&player.inventory().get_stack_in_hand(hand));
+        }
+
         self.player_placed(
             &world,
             placed_block,
