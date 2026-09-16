9db32f22971783365609f4cd2b3f9b3b330ec17d
Thijs226 <Thijsb226@gmail.com>
2026-09-16 18:22:43 +0200

    fix(block): play sound when nether portal breaks

--- crates/pumpkin/src/block/blocks/nether_portal.rs
+++ crates/pumpkin/src/block/blocks/nether_portal.rs
@@ -5,6 +5,7 @@ use pumpkin_data::{
     block_properties::{Axis, HorizontalAxis, NetherPortalLikeProperties},
     dimension::Dimension,
     entity::EntityType,
+    sound::{Sound, SoundCategory},
 };
 use pumpkin_macros::pumpkin_block;
 use pumpkin_util::{Difficulty, GameMode, math::vector3::Vector3};
@@ -65,6 +66,12 @@ impl BlockBehaviour for NetherPortalBlock {
         {
             return args.state_id;
         }
+
+        args.world.play_sound(
+            Sound::BlockGlassBreak,
+            SoundCategory::Blocks,
+            &args.position.to_centered_f64(),
+        );
         Block::AIR.default_state.id
     }
 
