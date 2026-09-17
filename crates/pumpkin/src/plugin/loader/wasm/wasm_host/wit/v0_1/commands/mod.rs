7f6e07dd86179f5bb034fbf84924f63ffbe23802
Thijs226 <Thijsb226@gmail.com>
2026-09-17 12:16:07 +0200

    feat(plugin): wire command requirement handlers

--- crates/pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1/commands/mod.rs
+++ crates/pumpkin/src/plugin/loader/wasm/wasm_host/wit/v0_1/commands/mod.rs
@@ -31,7 +31,9 @@ use crate::{
             PluginHostState, ServerResource, TextComponentResource, WasmCommand, WasmCommandNode,
         },
         wit::v0_1::{
-            commands::executor::{WasmCommandExecutor, WasmCommandSuggestionProvider},
+            commands::executor::{
+                WasmCommandExecutor, WasmCommandRequirement, WasmCommandSuggestionProvider,
+            },
             pumpkin::{
                 self,
                 plugin::{
@@ -267,7 +269,6 @@ impl pumpkin::plugin::command::HostConsumedArgs for PluginHostState {
                     pumpkin::plugin::command::EntityAnchor::Feet
                 }
             }),
-            // These types don't have direct WIT resource mappings yet
             OwnedArg::Entities(_) | OwnedArg::Entity(_) | OwnedArg::GameProfiles(_) => {
                 Arg::Simple(String::new())
             }
@@ -762,12 +763,31 @@ impl pumpkin::plugin::command::HostCommandNode for PluginHostState {
 
     async fn require_with_handler_id(
         &mut self,
-        _node: Resource<CommandNode>,
-        _handler_id: u32,
+        node: Resource<CommandNode>,
+        handler_id: u32,
     ) -> wasmtime::Result<()> {
-        Err(wasmtime::Error::msg(
-            "require_with_handler_id not implemented",
-        ))
+        let plugin = self
+            .plugin
+            .as_ref()
+            .and_then(std::sync::Weak::upgrade)
+            .ok_or_else(|| wasmtime::Error::msg("Plugin dropped"))?;
+        let server = self
+            .server
+            .clone()
+            .ok_or_else(|| wasmtime::Error::msg("Server not initialized"))?;
+
+        let requirement = WasmCommandRequirement {
+            handler_id,
+            plugin,
+            server,
+        };
+        let resource = self.get_node_mut(&node)?;
+        let builder = std::mem::replace(
+            &mut resource.provider,
+            WasmCommandNode::Literal(literal("")),
+        );
+        resource.provider = builder.requires(move |source| requirement.evaluate(source));
+        Ok(())
     }
 
     async fn drop(&mut self, rep: Resource<CommandNode>) -> wasmtime::Result<()> {
