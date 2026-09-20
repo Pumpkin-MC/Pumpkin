595bd98fbba1994e88b68a431297f8df949bbfc3
CocofireHD <cocofirehd@gmail.com>
2026-09-20 18:25:34 +0200

    fix(block): keep copper trapdoor state when waxing

--- crates/pumpkin/src/item/items/honeycomb.rs
+++ crates/pumpkin/src/item/items/honeycomb.rs
@@ -7,13 +7,13 @@ use crate::entity::player::Player;
 use crate::item::{ItemBehaviour, ItemMetadata};
 use crate::server::Server;
 use crate::world::World;
-use pumpkin_data::block_properties::OakDoorLikeProperties;
+use pumpkin_data::block_properties::{OakDoorLikeProperties, OakTrapdoorLikeProperties};
 use pumpkin_data::item::Item;
 use pumpkin_data::item_stack::ItemStack;
 use pumpkin_data::tag::Taggable;
 use pumpkin_data::world::WorldEvent;
 use pumpkin_data::{Block, tag};
-use pumpkin_data::{BlockDirection, BlockId};
+use pumpkin_data::{BlockDirection, BlockId, BlockStateId};
 use pumpkin_util::math::position::BlockPos;
 use pumpkin_util::math::vector3::Vector3;
 use pumpkin_world::world::BlockFlags;
@@ -59,25 +59,40 @@ pub(crate) fn try_wax_block(world: &Arc<World>, location: BlockPos, block: &Bloc
     };
     let new_block = replacement.to_block();
 
-    let new_state_id = if block.has_tag(&tag::Block::MINECRAFT_DOORS) {
-        // Carry the door state over to the waxed door.
-        let door_information = world.get_block_state_id(&location);
-        let door_props = OakDoorLikeProperties::from_state_id(door_information);
-        let mut new_door_properties = OakDoorLikeProperties::default(new_block);
+    let from_state_id = world.get_block_state_id(&location);
+    let new_state_id = waxed_state_id(block, from_state_id, new_block);
+
+    world.set_block_state(&location, new_state_id, BlockFlags::NOTIFY_ALL);
+    world.sync_world_event(WorldEvent::ParticlesWaxOn, location, 0);
+    true
+}
+
+fn waxed_state_id(
+    from_block: &Block,
+    from_state_id: BlockStateId,
+    to_block: &Block,
+) -> BlockStateId {
+    if from_block.has_tag(&tag::Block::MINECRAFT_DOORS) {
+        let door_props = OakDoorLikeProperties::from_state_id(from_state_id);
+        let mut new_door_properties = OakDoorLikeProperties::default(to_block);
         new_door_properties.facing = door_props.facing;
         new_door_properties.open = door_props.open;
         new_door_properties.half = door_props.half;
         new_door_properties.hinge = door_props.hinge;
         new_door_properties.powered = door_props.powered;
-        new_door_properties.to_state_id(new_block)
+        new_door_properties.to_state_id(to_block)
+    } else if from_block.has_tag(&tag::Block::MINECRAFT_TRAPDOORS) {
+        let trapdoor_props = OakTrapdoorLikeProperties::from_state_id(from_state_id);
+        let mut new_trapdoor_properties = OakTrapdoorLikeProperties::default(to_block);
+        new_trapdoor_properties.facing = trapdoor_props.facing;
+        new_trapdoor_properties.half = trapdoor_props.half;
+        new_trapdoor_properties.open = trapdoor_props.open;
+        new_trapdoor_properties.powered = trapdoor_props.powered;
+        new_trapdoor_properties.waterlogged = trapdoor_props.waterlogged;
+        new_trapdoor_properties.to_state_id(to_block)
     } else {
-        // TODO: Also carry over the properties of trapdoors.
-        new_block.default_state.id
-    };
-
-    world.set_block_state(&location, new_state_id, BlockFlags::NOTIFY_ALL);
-    world.sync_world_event(WorldEvent::ParticlesWaxOn, location, 0);
-    true
+        to_block.default_state.id
+    }
 }
 
 impl HoneyCombItem {
@@ -139,5 +154,61 @@ const fn get_waxed_equivalent(id: BlockId) -> Option<BlockId> {
         BlockId::EXPOSED_COPPER_TRAPDOOR => Some(BlockId::WAXED_EXPOSED_COPPER_TRAPDOOR),
         BlockId::COPPER_TRAPDOOR => Some(BlockId::WAXED_COPPER_TRAPDOOR),
         _ => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pumpkin_data::block_properties::{Half, HorizontalFacing};
+
+    #[test]
+    fn waxing_preserves_trapdoor_properties() {
+        for (from, to) in [
+            (BlockId::COPPER_TRAPDOOR, BlockId::WAXED_COPPER_TRAPDOOR),
+            (
+                BlockId::EXPOSED_COPPER_TRAPDOOR,
+                BlockId::WAXED_EXPOSED_COPPER_TRAPDOOR,
+            ),
+            (
+                BlockId::WEATHERED_COPPER_TRAPDOOR,
+                BlockId::WAXED_WEATHERED_COPPER_TRAPDOOR,
+            ),
+            (
+                BlockId::OXIDIZED_COPPER_TRAPDOOR,
+                BlockId::WAXED_OXIDIZED_COPPER_TRAPDOOR,
+            ),
+        ] {
+            let from_block = from.to_block();
+            let to_block = to.to_block();
+
+            let mut props = OakTrapdoorLikeProperties::default(from_block);
+            props.facing = HorizontalFacing::East;
+            props.half = Half::Top;
+            props.open = true;
+            props.powered = true;
+            props.waterlogged = true;
+            let state_id = props.to_state_id(from_block);
+
+            let waxed = waxed_state_id(from_block, state_id, to_block);
+            assert_eq!(Block::from_state_id(waxed), to_block);
+
+            let waxed_props = OakTrapdoorLikeProperties::from_state_id(waxed);
+            assert_eq!(waxed_props.facing, HorizontalFacing::East);
+            assert_eq!(waxed_props.half, Half::Top);
+            assert!(waxed_props.open);
+            assert!(waxed_props.powered);
+            assert!(waxed_props.waterlogged);
+        }
+    }
+
+    #[test]
+    fn waxing_other_blocks_uses_default_state() {
+        let waxed = waxed_state_id(
+            &Block::COPPER_BLOCK,
+            Block::COPPER_BLOCK.default_state.id,
+            &Block::WAXED_COPPER_BLOCK,
+        );
+        assert_eq!(waxed, Block::WAXED_COPPER_BLOCK.default_state.id);
     }
 }
