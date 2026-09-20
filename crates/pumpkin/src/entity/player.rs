fb9b10580555c33286cf24d0295694ec45f59c6f
CocofireHD <cocofirehd@gmail.com>
2026-09-20 18:25:40 +0200

    fix(entity): pass the attacker position to melee damage

--- crates/pumpkin/src/entity/player.rs
+++ crates/pumpkin/src/entity/player.rs
@@ -1384,7 +1384,7 @@ impl Player {
             } else {
                 DamageType::PLAYER_ATTACK
             },
-            None,
+            Some(self.living_entity.entity.pos.load()),
             Some(self),
             Some(self),
         ) {
@@ -1483,7 +1483,7 @@ impl Player {
                                 other_victim.as_ref(),
                                 sweep_damage,
                                 DamageType::PLAYER_ATTACK,
-                                None,
+                                Some(self.living_entity.entity.pos.load()),
                                 Some(self),
                                 Some(self),
                             );
